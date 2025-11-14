90bb3a573cf67b05115b25d2a11cbdbfdac591e9
Alex Small <alex.small739@gmail.com>
2025-11-14 18:40:50 +0000

    Connect server to db, test api welcome message.

--- server/server.ts
+++ server/server.ts
@@ -1,23 +1,48 @@
 import express, { Request, Response } from 'express';
 import next from "next";
+import mongoose from "mongoose";
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    const server = express();
 
+    const server = express();
+    server.use(express.json());
+
+    // Welcome message
     server.get('/api', (_req: Request, res: Response) => {
-        res.send('Welcome to The Dream Note API.');
+        res.json({message: 'Welcome to The Dream Note API.'});
     });
 
-    server.all("*", (req, res) => handle(req, res));
+    // Connect to MongoDB database
+    async function bootstrap() {
+        if (!process.env.DATABASE_URL){
+            throw new Error('Database url not found.')
+        }
+        try {
+            await mongoose.connect(
+                process.env.DATABASE_URL,
+            {
+                dbName: process.env.DATABASE_NAME,
+            }
+            );
+            console.log("Connnected To MongoDB");
+        } catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
+    }
 
+    server.all("*", (req, res) => handle(req, res));
 
     const port = process.env.PORT || 3000;
 
     server.listen(port, () => {
         console.log(`Server started on port ${port}`);
     });
+
+    bootstrap()
+
 })
