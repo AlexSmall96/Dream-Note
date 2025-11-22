c2226352e3d5d312e69ce91b72e52b9c0acd1233
Alex Small <alex.small739@gmail.com>
2025-11-22 09:09:02 +0000

    Add mock API response if DEV flag is true

--- server/services/dream.service.ts
+++ server/services/dream.service.ts
@@ -17,20 +17,33 @@ export class DreamService {
     }
 
     // Method to get title from openAI API
-    public async generateAIDreamInfo(description: string, prompt: string, forThemes: boolean){
-        const response = await this.openAI.responses.create({
+    public async generateAIDreamInfo(description: string, prompt: string, forThemes: boolean, analysisOptions?: {tone: string, style: string}){
+        // If options have been passed in to set analysis, add to system prompt
+        const  fullPrompt = analysisOptions? prompt + analysisOptions.tone + analysisOptions.style : prompt
+        // Create 
+        const response = !process.env.DEV? await this.openAI.responses.create({
             model: "gpt-5-nano",
             input: [
                 {
                     role: 'system',
-                    content: prompt
+                    content: fullPrompt
                 }, {
                     role: 'user',
                     content: description
                 }
             ]
-        }); 
-
+        }) : {
+            // If DEV flag is supplied, mock response 
+            // If analysis - supply generic analysis referring to tone and style
+            output_text: analysisOptions? 
+                `Mock analysis response. Tone: ${analysisOptions.tone}. Style: ${analysisOptions.style}.` 
+            // If themes, return generic themes
+                : forThemes? 
+                    'theme1,theme2,theme3'
+                : 
+                    // If title - use first 30 characters of description
+                    description.substring(0, 30)
+        }
         if (forThemes){
             // Return themes as array
             return response.output_text.trim().split(',') ?? []
