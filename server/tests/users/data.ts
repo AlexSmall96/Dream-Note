d6b32bba25647bd5af7d866e405a7fa255ac4175
Alex Small <alex.small739@gmail.com>
2026-02-11 10:39:13 +0000

    Update users test data file with createUser function to dynamically create user with new token and id,

--- server/tests/users/data.ts
+++ server/tests/users/data.ts
@@ -1,56 +1,52 @@
-import mongoose from 'mongoose';
+import mongoose, { Types } from 'mongoose';
 import jwt from 'jsonwebtoken';
 import { User } from '../../models/user.model.js';
 
-// Create user ids 
-const userOneId = new mongoose.Types.ObjectId()
-const userThreeId = new mongoose.Types.ObjectId()
-const userFourId = new mongoose.Types.ObjectId()
-
-// Create token
 const JWT_SECRET = process.env.JWT_SECRET
 
-const generateToken = (id: string) => {
+const generateToken = (_id: string, isGuest: boolean = false) => {
     if (!JWT_SECRET){
         throw new Error('Please provide a json web token secret key.')
     }
-    return jwt.sign({ _id:id }, JWT_SECRET, { expiresIn: "24h" })
+    return jwt.sign({ _id, isGuest }, JWT_SECRET, { expiresIn: "24h" })
 }
 
-// Create users to save to DB
-const userOne = {
-    email: 'user1@email.com',
-    password: 'apple123',
-    _id: userOneId,
-    tokens: [generateToken(userOneId.toString())]
+type userType = {
+    email: string,
+    password: string,
+    _id: Types.ObjectId,
+    tokens: string[],
 }
 
-const userThree = {
-    email: 'user3@email.com',
-    password: 'orange123',
-    _id: userThreeId,
-    tokens: [generateToken(userThreeId.toString())]
+// Dynamically create a user by generating token and id
+const createUser = async ({email, password, isGuest = false, withTokens = true}:{email: string, password: string, isGuest?: boolean, withTokens?: boolean}): Promise<userType> => {
+    
+    // Create a fresh token and id each time for test reliability
+    const _id = new mongoose.Types.ObjectId()
+    const token = generateToken(_id.toString(), isGuest)
+    
+    const user = {
+        _id, email, password, tokens: withTokens ? [token] : []
+    }
+
+    await new User(user).save()
+
+    return user
 }
 
-const userFour = {
-    email: 'user4@email.com',
-    password: 'pear123',
-    _id: userFourId,
-    tokens: [generateToken(userFourId.toString())]
-}
+// Creates ready to use format from token for sending authenticated requests
+const getAuthHeader = (token: string): [string, string] => {
+    return ['Authorization', `Bearer ${token}`]
+} 
 
-// Use token to create auth string
-const userOneAuth: [string, string] = ['Authorization', `Bearer ${userOne.tokens[0]}`]
-const userThreeAuth: [string, string] = ['Authorization', `Bearer ${userThree.tokens[0]}`]
-const userFourAuth: [string, string] = ['Authorization', `Bearer ${userFour.tokens[0]}`]
+// Define credentials to save users in tests
+const userOneCreds = {email: 'user1@email.com', password: 'apple123'}
+const userThreeCreds = {email :'user3@email.com', password: 'orange123'}
+const userFourCreds = {email: 'user4@email.com', password: 'pear123'}
+const guestUserCreds = {email: 'demo-test@email.com', password: 'demotest123'}
 
-const saveUsers = async () => {
-    await new User(userOne).save()
-    await new User(userThree).save()
-    await new User(userFour).save()
-}
 
 export {
-    userOneId, userThreeId, userFourId, saveUsers, userOne, userOneAuth, userThreeAuth, userFourAuth
+    userOneCreds, userFourCreds, getAuthHeader, userType, createUser, userThreeCreds, guestUserCreds
 }
 
