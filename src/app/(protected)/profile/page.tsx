d85b423d6bf3bebf7e33fa336102d5e8eba73d36
Alex Small <alex.small739@gmail.com>
2026-02-12 15:58:56 +0000

    Conditonally render ProfileForm based on isGuest status

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,10 +1,20 @@
+"use client"
 import ProfileForm from "@/components/profile/ProfileForm";
+import { useCurrentUser } from "@/contexts/CurrentUserContext";
 
 export default function Profile(){
+    const {isGuest} = useCurrentUser()
+
     return (
         <div className="flex flex-col items-center">
             <h1 className="text-l font-bold m-4">Profile</h1>
-            <ProfileForm />
+            {!isGuest ?
+                <ProfileForm /> 
+            : 
+                <>
+                    Email and password updates and account deletion are disabled for guests. Create an account to access these features.
+                </>
+            }
         </div>
     )
 }
