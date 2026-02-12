0a6e476291c8f2e9aef9fdbdeeee626424978537
Alex Small <alex.small739@gmail.com>
2026-02-12 13:39:58 +0000

    Render null in aside if loading or no current user

--- src/app/(protected)/dreams/@aside/default.tsx
+++ src/app/(protected)/dreams/@aside/default.tsx
@@ -2,6 +2,7 @@
 import DreamsList from '@/components/dreams/DreamsList'
 import MonthsWithDreams from '@/components/dreams/MonthsWithDreams';
 import ThemesList from '@/components/themes/ThemesList';
+import { useCurrentUser } from '@/contexts/CurrentUserContext';
 import { useDreams } from '@/contexts/DreamsContext';
 import { useThemesAside } from '@/contexts/ThemesAsideContext';
 import { useRouter } from "next/navigation";
@@ -26,38 +27,43 @@ export default function Aside() {
         setSearch('')
     }
 
-    return (
-        <div>
-            <form className="flex flex-col gap-2 w-80 m-3 border-2 border-gray-300">
-                <input 
-                    type='text'
-                    value={search}
-                    onChange={handleSearch}
-                    placeholder='Search all dreams'
-                />
-                {searchResults.length > 0 && 
-                <div>
-                    {searchResults.map(dream => 
-                        <div key={dream._id} onClick={() => handleClick(dream._id)} className="col-span-2 hover:underline">{dream.title}</div>
-                    )}      
-                </div>}
-            </form>
+    const { loading, currentUser } = useCurrentUser()
 
-            <button 
-                onClick={() => router.replace(`/dreams/create`)} 
-                className='bg-blue-500 hover:bg-blue-700 text-white font-bold p-2'
-            >
-                Log New Dream
-            </button>
-            <button 
-                onClick={handleChangeView}
-                className='bg-gray-500 hover:bg-blue-700 text-white font-bold p-2'
-            >
-                {view === 'themes'? 'View by date': 'View by theme'}
-            </button>
-            {view === 'themes' && <ThemesList />}
-            {selectedTheme && <DreamsList />}
-            {view === 'dreams' && <MonthsWithDreams />}
-        </div>
+    return (
+        loading || !currentUser? 
+            null 
+        :
+            <div>
+                <form className="flex flex-col gap-2 w-80 m-3 border-2 border-gray-300">
+                    <input 
+                        type='text'
+                        value={search}
+                        onChange={handleSearch}
+                        placeholder='Search all dreams'
+                    />
+                    {searchResults.length > 0 && 
+                    <div>
+                        {searchResults.map(dream => 
+                            <div key={dream._id} onClick={() => handleClick(dream._id)} className="col-span-2 hover:underline">{dream.title}</div>
+                        )}      
+                    </div>}
+                </form>
+
+                <button 
+                    onClick={() => router.replace(`/dreams/create`)} 
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold p-2'
+                >
+                    Log New Dream
+                </button>
+                <button 
+                    onClick={handleChangeView}
+                    className='bg-gray-500 hover:bg-blue-700 text-white font-bold p-2'
+                >
+                    {view === 'themes'? 'View by date': 'View by theme'}
+                </button>
+                {view === 'themes' && <ThemesList />}
+                {selectedTheme && <DreamsList />}
+                {view === 'dreams' && <MonthsWithDreams />}
+            </div>
   )
 }
