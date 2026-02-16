30109bf0195ab9d1b8044787bcf517d62355f1b2
Alex Small <alex.small739@gmail.com>
2026-02-16 20:14:42 +0000

    Use ResetPasswordForm on new password page

--- src/app/auth/reset-password/new/page.tsx
+++ src/app/auth/reset-password/new/page.tsx
@@ -1,9 +1,13 @@
-import NewPasswordForm from "@/components/auth/NewPasswordForm"
+'use client'
+import ResetPasswordForm from "@/components/auth/ResetPasswordForm"
 
 export default function NewPasswordPage (){
     return (
         <div className="flex flex-col items-center m-4">
-            <NewPasswordForm />
+            <h1 className="m-2">
+                One time passcode correct. Enter your new password below.
+            </h1>
+            <ResetPasswordForm />
         </div>
     )
 }
