14d5985ee7c4a0489e9fb12b7be024dfc246d722
Alex Small <alex.small739@gmail.com>
2026-02-16 20:13:45 +0000

    Render EmailForm in update/email page with new props

--- src/app/(protected)/account/update/email/page.tsx
+++ src/app/(protected)/account/update/email/page.tsx
@@ -1,9 +1,16 @@
+"use client"
 import EmailForm from "@/components/account/EmailForm";
+import { requestEmailUpdate, verifyOTPAndUpdateEmail } from "@/lib/api/account";
 
 export default function UpdateEmail(){
+
     return (
         <div className="flex flex-col items-center m-4">
-            <EmailForm />   
+            <EmailForm<{otp: string}> 
+                requestFn={requestEmailUpdate} 
+                verifyFn={verifyOTPAndUpdateEmail} 
+                buildVerifyPayload={(otp, _email) => ({otp})}
+            />   
         </div>
     )
 }
