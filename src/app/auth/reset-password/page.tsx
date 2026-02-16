c574b25a33488a6fd8010af5e47343cd4dda8c5b
Alex Small <alex.small739@gmail.com>
2026-02-16 20:14:15 +0000

    Use EmailForm component in reset-password page

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,10 +1,16 @@
-import RequestPasswordReset from "@/components/auth/RequestPasswordReset"
+"use client"
+import { requestPasswordReset, verifyResetOTP } from "@/lib/api/account";
+import EmailForm from "@/components/account/EmailForm"
 
-export default function ResetPassword(){
+export default function RequestPasswordReset () {
+
     return (
         <div className="flex flex-col items-center m-4">
-            <RequestPasswordReset />  
+            <EmailForm<{otp: string, email: string}> 
+                requestFn={requestPasswordReset} 
+                verifyFn={verifyResetOTP} 
+                buildVerifyPayload={(otp, email) => ({otp, email})} 
+            />  
         </div>
-        
     )
 }
