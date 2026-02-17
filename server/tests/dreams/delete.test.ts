5dec098e1f59ab5e717ec83fe79ab7cecd43290a
Alex Small <alex.small739@gmail.com>
2026-02-17 09:53:05 +0000

    Use assertSingleError in dream/delete.test and remove describe block

--- server/tests/dreams/delete.test.ts
+++ server/tests/dreams/delete.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import { beforeEach, describe, expect, test } from 'vitest';
+import { beforeEach, expect, test } from 'vitest';
 import { wipeDB } from '../setup/wipeDB.js'
 import { server } from '../setup/testServer.js'
 import { userOneCreds, userThreeCreds } from '../users/data.js';
@@ -9,6 +9,7 @@ import { Dream } from '../../models/dream.model.js';
 import { baseUrl } from './utils.js';
 import { oldDreamData } from './data.js';
 import { Types } from 'mongoose';
+import { assertSingleError } from '../users/utils/assertErrors.js';
 
 let userOneAuth: [string, string]
 let userThreeAuth: [string, string]
@@ -29,34 +30,33 @@ beforeEach(async () => {
     oldDreamId = oldDream._id
 })
 
-// Delete dream
-describe('DELETE DREAM', () => {
 
-    // Define url
-    const url = baseUrl + '/delete'
+// Define url
+const url = baseUrl + '/delete'
 
-    test('User cannot delete dream they are not the owner of.', async () => {
-        // Attempt unauthorized deletion
-        const response = await request(server).delete(`${url}/${oldDreamId}`).set(...userOneAuth).expect(401)
-        expect(response.body.error).toBe('You are not authorized to delete this dream.')        
-    })
+// Tests
 
-    test('Dream deletion is successful if user is owner of dream, and associated themes are also deleted.', async () => {
-        // The 2 themes associated with dream: oldDreamId should be in db
-        const themeNames = ['Lateness', 'Anxiety']
-        await Promise.all(themeNames.map(async (theme: string) => {
-            const savedTheme = await Theme.find({theme, dream: oldDreamId})
-            expect(savedTheme).not.toBeNull()
-        }))
-        // Delete dream authorized as correct owner
-        await request(server).delete(`${url}/${oldDreamId}`).set(...userThreeAuth).expect(200)
-        // Dream should be removed from database
-        const dream = await Dream.findById(oldDreamId)
-        expect(dream).toBeNull()
-        // Associated themes should be removed
-        await Promise.all(themeNames.map(async (theme: string) => {
-            const nullTheme = await Theme.findOne({theme})
-            expect(nullTheme).toBeNull()
-        }))
-    })
+test('User cannot delete dream they are not the owner of.', async () => {
+    // Attempt unauthorized deletion
+    const response = await request(server).delete(`${url}/${oldDreamId}`).set(...userOneAuth).expect(403)
+    assertSingleError(response.body.errors, 'You are not authorized to delete this dream.')
+})
+
+test('Dream deletion is successful if user is owner of dream, and associated themes are also deleted.', async () => {
+    // The 2 themes associated with dream: oldDreamId should be in db
+    const themeNames = ['Lateness', 'Anxiety']
+    await Promise.all(themeNames.map(async (theme: string) => {
+        const savedTheme = await Theme.find({theme, dream: oldDreamId})
+        expect(savedTheme).not.toBeNull()
+    }))
+    // Delete dream authorized as correct owner
+    await request(server).delete(`${url}/${oldDreamId}`).set(...userThreeAuth).expect(200)
+    // Dream should be removed from database
+    const dream = await Dream.findById(oldDreamId)
+    expect(dream).toBeNull()
+    // Associated themes should be removed
+    await Promise.all(themeNames.map(async (theme: string) => {
+        const nullTheme = await Theme.findOne({theme})
+        expect(nullTheme).toBeNull()
+    }))
 })
