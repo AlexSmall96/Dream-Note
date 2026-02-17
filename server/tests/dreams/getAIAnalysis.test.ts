006362603583d2c72223ff1820dfe2b00d43248f
Alex Small <alex.small739@gmail.com>
2026-02-17 09:54:21 +0000

    Use assertSingleError in getAIAnalysis.tests and remove describe block

--- server/tests/dreams/getAIAnalysis.test.ts
+++ server/tests/dreams/getAIAnalysis.test.ts
@@ -5,6 +5,7 @@ import { server } from '../setup/testServer.js'
 import { userOneCreds } from '../users/data.js';
 import {createUser, getAuthHeader} from '../users/utils/userCreation.js'
 import { baseUrl } from './utils.js';
+import { assertSingleError } from '../users/utils/assertErrors.js';
 
 let userOneAuth: [string, string]
 
@@ -17,22 +18,22 @@ beforeEach(async () => {
     userOneAuth = getAuthHeader(userOne.tokens[0])
 })
 
-describe('GET ANALYSIS', () => {
-    // Define url
-    const url = baseUrl + '/analysis' 
+// Define url
+const url = baseUrl + '/analysis' 
 
-    test('Generate analysis fails if dream description is not provided.', async () => {
-        // Error should be returned
-        const response = await request(server).post(url).send({dream: {title: 'A dream title'}}).set(...userOneAuth).expect(400)
-        expect(response.body.error).toBe("Description must be provided.")
-    })
-    test('Mock response is returned if dream description is provided.', async () => {
-        // Mock response is returned based on tone and style parameters
-        const response = await request(server).post(url).send({
-            description: 'A dream description',
-            tone: 'serious',
-            style: 'formal'
-        }).set(...userOneAuth).expect(200)
-        expect(response.body.analysis).toBe('Mock analysis response. Description: A dream description Tone: serious, Style: formal.')
-    })  
+// Tests
+
+test('Generate analysis fails if dream description is not provided.', async () => {
+    // Error should be returned
+    const response = await request(server).post(url).send({dream: {title: 'A dream title'}}).set(...userOneAuth).expect(400)
+    assertSingleError(response.body.errors, 'Description must be provided.', 'description')
 })
+test('Mock response is returned if dream description is provided.', async () => {
+    // Mock response is returned based on tone and style parameters
+    const response = await request(server).post(url).send({
+        description: 'A dream description',
+        tone: 'serious',
+        style: 'formal'
+    }).set(...userOneAuth).expect(200)
+    expect(response.body.analysis).toBe('Mock analysis response. Description: A dream description Tone: serious, Style: formal.')
+})  
