e8394b34ec749f2ca1cbb85f20a34c91455de402
Alex Small <alex.small739@gmail.com>
2026-02-17 09:55:19 +0000

    Remove describe block from getDreams.test

--- server/tests/dreams/getDreams.test.ts
+++ server/tests/dreams/getDreams.test.ts
@@ -55,58 +55,57 @@ beforeEach(async () => {
 
 })
 
-// Get dreams
-describe('GET ALL DREAMS', () => {
-    // Define url
-    const url = baseUrl
-    
-    test("All dreams should be returned when no parameters are passed in.", async () => {
-        // Get all userOne's dreams
-        const response = await request(server).get(`${url}`).set(...userOneAuth).expect(200)
-        expect(response.body.dreams).toHaveLength(9)
-    })
+// Define url
+const url = baseUrl
 
-    test("Skip, limit and title parameters return correct dreams.", async () => {
-        // Get dream page one of dreams
-        const pageOneResponse = await request(server).get(`${url}?limit=5&skip=0&year=2025&month=6`).set(...userOneAuth).expect(200)
-        // Should be 5 dreams, sorted oldest to newest
-        const pageOneDreams = pageOneResponse.body.dreams
-        assertDreamTitlesAndDates(pageOneDreams, 5)
-        // Get dream page two of dreams
-        const pageTwoResponse = await request(server).get(`${url}?limit=5&skip=5&year=2025&month=6`).set(...userOneAuth).expect(200)
-        // Should be 4 dreams, starting at dream4, sorted oldest to newest
-        const pageTwoDreams = pageTwoResponse.body.dreams
-        assertDreamTitlesAndDates(pageTwoDreams, 4, 4)
-        // Should only return dream9
-        const singleResponse = await request(server).get(`${url}?limit=5&skip=0&title=dream9&year=2025&month=6`).set(...userOneAuth).expect(200)
-        const singleDreamArray = singleResponse.body.dreams
-        expect(singleDreamArray).toHaveLength(1)
-        expect(singleDreamArray[0].title).toBe('dream9')
-    })
+// Tests
 
-    test('Searching by title returns correct dreams.', async () => {
-        // Searching 'In space should return all 3 of userThree dreams
-        const allDreamsResponse = await request(server).get(`${url}?title=In space`).set(...userThreeAuth).expect(200)
-        const allDreams = allDreamsResponse.body.dreams
-        expect(allDreams).toHaveLength(3)
-        // Searching 'In space without' should return 1 dream
-        const singleDreamResponse = await request(server).get(`${url}?title=In space without`).set(...userThreeAuth).expect(200)
-        const singleDream = singleDreamResponse.body.dreams
-        expect(singleDream).toHaveLength(1)
-        expect(singleDream[0].title).toBe('In space without a space suit')
-        // Searching 'space suit' should return 2 dreams
-        const twoDreamResponse = await request(server).get(`${url}?title=space suit`).set(...userThreeAuth).expect(200)
-        const twoDreams = twoDreamResponse.body.dreams
-        expect(twoDreams).toHaveLength(2)
-        // Should be sorted newest to oldest
-        expect(twoDreams[0].title).toBe('In space wearing a space suit')
-        expect(twoDreams[1].title).toBe('In space without a space suit')
-    })
+test("All dreams should be returned when no parameters are passed in.", async () => {
+    // Get all userOne's dreams
+    const response = await request(server).get(`${url}`).set(...userOneAuth).expect(200)
+    expect(response.body.dreams).toHaveLength(9)
+})
 
-    test('Setting month and year returns correct dreams.', async () => {
-        await filterAndAssertDreams(2024, 12, 1, userThreeAuth,  [oldDreamTitle])
-        await filterAndAssertDreams(2025, 5, 1, userThreeAuth, [newDreamTitle])
-        await filterAndAssertDreams(2025, 6, 3, userThreeAuth, ['In space wearing a space suit', 'In space without a space suit', 'In space'])
-        await filterAndAssertDreams(2020, 2, 0, userThreeAuth)
-    })
+test("Skip, limit and title parameters return correct dreams.", async () => {
+    // Get dream page one of dreams
+    const pageOneResponse = await request(server).get(`${url}?limit=5&skip=0&year=2025&month=6`).set(...userOneAuth).expect(200)
+    // Should be 5 dreams, sorted oldest to newest
+    const pageOneDreams = pageOneResponse.body.dreams
+    assertDreamTitlesAndDates(pageOneDreams, 5)
+    // Get dream page two of dreams
+    const pageTwoResponse = await request(server).get(`${url}?limit=5&skip=5&year=2025&month=6`).set(...userOneAuth).expect(200)
+    // Should be 4 dreams, starting at dream4, sorted oldest to newest
+    const pageTwoDreams = pageTwoResponse.body.dreams
+    assertDreamTitlesAndDates(pageTwoDreams, 4, 4)
+    // Should only return dream9
+    const singleResponse = await request(server).get(`${url}?limit=5&skip=0&title=dream9&year=2025&month=6`).set(...userOneAuth).expect(200)
+    const singleDreamArray = singleResponse.body.dreams
+    expect(singleDreamArray).toHaveLength(1)
+    expect(singleDreamArray[0].title).toBe('dream9')
+})
+
+test('Searching by title returns correct dreams.', async () => {
+    // Searching 'In space should return all 3 of userThree dreams
+    const allDreamsResponse = await request(server).get(`${url}?title=In space`).set(...userThreeAuth).expect(200)
+    const allDreams = allDreamsResponse.body.dreams
+    expect(allDreams).toHaveLength(3)
+    // Searching 'In space without' should return 1 dream
+    const singleDreamResponse = await request(server).get(`${url}?title=In space without`).set(...userThreeAuth).expect(200)
+    const singleDream = singleDreamResponse.body.dreams
+    expect(singleDream).toHaveLength(1)
+    expect(singleDream[0].title).toBe('In space without a space suit')
+    // Searching 'space suit' should return 2 dreams
+    const twoDreamResponse = await request(server).get(`${url}?title=space suit`).set(...userThreeAuth).expect(200)
+    const twoDreams = twoDreamResponse.body.dreams
+    expect(twoDreams).toHaveLength(2)
+    // Should be sorted newest to oldest
+    expect(twoDreams[0].title).toBe('In space wearing a space suit')
+    expect(twoDreams[1].title).toBe('In space without a space suit')
+})
+
+test('Setting month and year returns correct dreams.', async () => {
+    await filterAndAssertDreams(2024, 12, 1, userThreeAuth,  [oldDreamTitle])
+    await filterAndAssertDreams(2025, 5, 1, userThreeAuth, [newDreamTitle])
+    await filterAndAssertDreams(2025, 6, 3, userThreeAuth, ['In space wearing a space suit', 'In space without a space suit', 'In space'])
+    await filterAndAssertDreams(2020, 2, 0, userThreeAuth)
 })
