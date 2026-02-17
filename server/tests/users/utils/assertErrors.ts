2b9b6b2ebc66edf9de7831846d6f71845af59f6d
Alex Small <alex.small739@gmail.com>
2026-02-17 09:30:01 +0000

    Add assertSingleError function to assertErrors test util

--- server/tests/users/utils/assertErrors.ts
+++ server/tests/users/utils/assertErrors.ts
@@ -3,11 +3,18 @@ import { expect } from 'vitest';
 
 // Helper function to make assertions on email, password and token errors
 export const assertErrors = (
-    errorsResponse: {param: string, msg: string}[], 
-    errorMsgs : {param: string, msg: string}[]
+    errorsResponse: {param?: string, msg: string}[], 
+    errorMsgs : {param?: string, msg: string}[]
 ) => {
     expect(errorsResponse).toHaveLength(errorMsgs.length)
     errorsResponse.forEach((error, index) => {
         expect(error).toMatchObject(errorMsgs[index])
     })
+}
+
+export const assertSingleError = (
+    errorsResponse: {param?: string, msg: string}[], msg: string, param?: string
+) => {
+    expect(errorsResponse).toHaveLength(1)
+    expect(errorsResponse[0]).toMatchObject(param? {msg, param}: {msg})
 }
