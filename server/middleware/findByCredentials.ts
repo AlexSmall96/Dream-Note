12c15a2b825eee772fe681bd95602851517397d4
Alex Small <alex.small739@gmail.com>
2026-02-18 19:04:26 +0000

    Update findByCredentials middleware using AppError

--- server/middleware/findByCredentials.ts
+++ server/middleware/findByCredentials.ts
@@ -1,23 +1,24 @@
 import { User } from '../models/user.model.js'
-import { AuthenticatedRequest } from "../interfaces/auth.interfaces.js"
+import { RequestWithUser } from "../interfaces/auth.interfaces.js"
 import { NextFunction, Response } from "express"
 import bcrypt from "bcrypt";
+import { AppError } from '../utils/appError.js';
 
 // Middleware to find a user by email address and password
 // Used in login router
-export const findByCredentials = async (req:AuthenticatedRequest, res: Response, next: NextFunction) => {
+export const findByCredentials = async (req:RequestWithUser, res: Response, next: NextFunction) => {
     // Destructure email and password
     const { email, password } = req.body
     try {
         // Check if user exists with email address
         const user = await User.findOne({email})
         if (!user){
-            return res.status(400).json({errors: [{param: 'email', msg: 'No account found associated with provided email address.'}]})
+            throw new AppError('No account found associated with provided email address.', 400, 'email')
         }
         // Check if password is correct
         const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch){
-            return res.status(400).json({errors: [{param: 'password', msg: 'Incorrect password.'}]})
+            throw new AppError('Incorrect password.', 400, 'password')
         }
         // If both checks pass, add found user to request 
         req.user = user
