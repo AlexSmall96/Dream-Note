ec2414b8032c7bb1314c775bf1b0da86487b6901
Alex Small <alex.small739@gmail.com>
2026-02-18 19:03:53 +0000

    Create forbidGuest middleware to throw error when guests attempts forbidden actions

---
+++ server/middleware/forbidGuest.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Response } from 'express';
+import { IncomingAuthRequest} from "../interfaces/auth.interfaces.js"
+import { AppError } from '../utils/appError.js';
+
+export function forbidGuest(message: string) {
+    return function(req:IncomingAuthRequest, res: Response, next: NextFunction) {
+        try {
+            // Should always be used after auth middleware
+            if (req.isGuest) {
+                throw new AppError(message, 403)
+            }
+            next()             
+        } catch (err){
+            next(err)
+        }
+    }
+}
