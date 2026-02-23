7ce114721189b9ffde829e4c3d5f64431570e0fd
Alex Small <alex.small739@gmail.com>
2026-02-23 13:54:14 +0000

    Add tests for missing tone and style options in getAIAnalysis tests

--- server/tests/dreams/getAIAnalysis.test.ts
+++ server/tests/dreams/getAIAnalysis.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import { beforeEach, describe, expect, test } from 'vitest';
+import { beforeEach, expect, test } from 'vitest';
 import { wipeDB } from '../setup/wipeDB.js'
 import { server } from '../setup/testServer.js'
 import { userOneCreds } from '../users/data.js';
@@ -25,15 +25,48 @@ const url = baseUrl + '/analysis'
 
 test('Generate analysis fails if dream description is not provided.', async () => {
     // Error should be returned
-    const response = await request(server).post(url).send({dream: {title: 'A dream title'}}).set(...userOneAuth).expect(400)
+    const response = await request(server).post(url).send({title: 'A dream title'}).set(...userOneAuth).expect(400)
     assertSingleError(response.body.errors, 'Description must be provided.', 'description')
 })
-test('Mock response is returned if dream description is provided.', async () => {
+
+test('Correct mock response is returned if dream description and tone and style parameters are provided.', async () => {
     // Mock response is returned based on tone and style parameters
     const response = await request(server).post(url).send({
-        description: 'A dream description',
-        tone: 'serious',
-        style: 'formal'
+        description: 'A dream description.',
+        params: {
+            tone: 'serious',
+            style: 'formal'            
+        }
     }).set(...userOneAuth).expect(200)
-    expect(response.body.analysis).toBe('Mock analysis response. Description: A dream description Tone: serious, Style: formal.')
+    expect(response.body.analysis).toBe('Mock analysis response. Description: A dream description. Tone: serious. Style: formal.')
+}) 
+
+test('Correct mock response is returned if dream description and only tone parameter is provided.', async () => {
+    // Mock response is returned based on tone and style parameters
+    const response = await request(server).post(url).send({
+        description: 'A dream description.',
+        params: {
+            tone: 'serious',         
+        }
+    }).set(...userOneAuth).expect(200)
+    expect(response.body.analysis).toBe('Mock analysis response. Description: A dream description. Tone: serious. Style: No style provided.')
+}) 
+
+test('Correct mock response is returned if dream description and only tone parameter is provided.', async () => {
+    // Mock response is returned based on tone and style parameters
+    const response = await request(server).post(url).send({
+        description: 'A dream description.',
+        params: {
+            style: 'formal',         
+        }
+    }).set(...userOneAuth).expect(200)
+    expect(response.body.analysis).toBe('Mock analysis response. Description: A dream description. Tone: No tone provided. Style: formal.')
 })  
+
+test('Correct mock response is returned if dream description and no ai parameters are provided.', async () => {
+    // Mock response is returned based on tone and style parameters
+    const response = await request(server).post(url).send({
+        description: 'A dream description.',
+    }).set(...userOneAuth).expect(200)
+    expect(response.body.analysis).toBe('Mock analysis response. Description: A dream description.')    
+})
