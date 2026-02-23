50a95c6bb3992009c3c12ae639a3f2e190f035e6
Alex Small <alex.small739@gmail.com>
2026-02-23 13:49:22 +0000

    Create forbidNotOwner middleware for unauthorized dream view, update and delete

---
+++ server/middleware/dreams/forbidNotOwner.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Response } from 'express';
+import { AuthenticatedRequest, IncomingAuthRequest} from "../../interfaces/auth.interfaces.js"
+import { AppError } from '../../utils/appError.js';
+import { Dream } from '../../models/dream.model.js';
+
+export function forbidNotOwner(message: string) {
+    return async function(req:IncomingAuthRequest, _res: Response, next: NextFunction) {
+        const dreamId = req.params.id
+        const authReq = req as AuthenticatedRequest
+        const owner = authReq.user._id
+        try {
+            const dream = await Dream.findOne({owner, _id: dreamId})
+            if (!dream){
+                throw new AppError(message, 403)
+            }
+            next()             
+        } catch (err){
+            next(err)
+        }
+    }
+}
