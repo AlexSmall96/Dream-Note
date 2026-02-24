b6648c01a6087edfa63b63e41612f68f4885f546
Alex Small <alex.small739@gmail.com>
2026-02-24 18:38:10 +0000

    Update forbidNotOwner middleware to allow flexible id names

--- server/middleware/dreams/forbidNotDreamOwner.ts
+++ server/middleware/dreams/forbidNotDreamOwner.ts
@@ -5,7 +5,10 @@ import { Dream } from '../../models/dream.model.js';
 
 export function forbidNotDreamOwner(message: string) {
     return async function(req:IncomingAuthRequest, _res: Response, next: NextFunction) {
-        const dreamId = req.params.id
+        const dreamId = req.params.dreamId ?? req.params.id
+        if (!dreamId) {
+            throw new AppError('Dream id not provided.', 400)
+        }
         const authReq = req as AuthenticatedRequest
         const owner = authReq.user._id
         try {
