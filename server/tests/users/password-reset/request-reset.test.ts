81ce6886325ac92a0f8f96f0bf1f0ed2ddef213b
Alex Small <alex.small739@gmail.com>
2026-02-26 07:47:20 +0000

    Add test that otp record gets created in password reset tests

--- server/tests/users/password-reset/request-reset.test.ts
+++ server/tests/users/password-reset/request-reset.test.ts
@@ -7,12 +7,16 @@ import { baseUrl } from '../data.js';
 import { createUser } from '../utils/userCreation.js';
 import { userOneCreds } from '../data.js';
 import { assertSingleError } from '../utils/assertErrors.js';
+import bcrypt from "bcrypt";
+
+let userOneId: string
 
 // Wipe db and save data
 beforeEach(async () => {
     await wipeDB()
     // Create a user to test success for valid email address
-    await createUser(userOneCreds)
+    const userOne = await createUser(userOneCreds)
+    userOneId = userOne._id.toString()
     vi.clearAllMocks() // Reset number of send mail calls
 })
 
@@ -33,16 +37,26 @@ vi.spyOn(otpUtils, "generateOtp").mockReturnValue('123456');
 
 // Import server after mock
 import { server } from '../../setup/testServer.js'
+import { Otp } from '../../../models/OTP.model.js';
 
 
 const url = baseUrl + '/request-password-reset'
 
 // Tests
 
-describe('STATUS & FEEDBACK MESSAGES', () => {
-    test('Error message should be returned if email is not provided.', async () => {
+describe('Requesting password reset should fail if:', () => {
+    test('Email is not provided.', async () => {
         const response = await request(server).post(url).send({}).expect(400)
         assertSingleError(response.body.errors, 'Email required', 'email')
+    })
+
+    test('Email is not associated with any user. Message is still sent to avoid email enumeration.', async () => {
+        const sendMailMock = nodemailer.createTransport().sendMail;
+        // Use unsaved address - sendMailMock doesnt get called
+        const response = await request(server).post(url).send({email: 'example@email.com'}).expect(200)
+        expect(sendMailMock).toHaveBeenCalledTimes(0);
+        // Feedback message is returned regardless of wether or not email was sent
+        expect(response.body.message).toBe("If an account is associated with the provided email address, a OTP will be sent.")
     })
 
     test('Correct message is returned if email is provided.', async () => {
@@ -51,11 +65,16 @@ describe('STATUS & FEEDBACK MESSAGES', () => {
     })
 })
 
-describe('EMAIL MOCKS.', async () => {
-    test('Email is sent if provided address is associated with a user.', async () => {
-        const sendMailMock = nodemailer.createTransport().sendMail;
+describe('Requesting password reset should succeed if:', async () => {
+    test('Provided email address is associated with a user.', async () => {
+        // Assert to otp is not saved in db yet
+        const nullOtp = await Otp.find()
+        expect(nullOtp).toHaveLength(0)   
+        
         // Use userOne's email - a saved address to assert sendMailMock is called once
-        await request(server).post(url).send({email: userOneCreds.email}).expect(200)
+        const sendMailMock = nodemailer.createTransport().sendMail;
+        const response = await request(server).post(url).send({email: userOneCreds.email}).expect(200)
+        expect(response.body.message).toBe("If an account is associated with the provided email address, a OTP will be sent.")
         expect(sendMailMock).toHaveBeenCalledTimes(1);
         expect(sendMailMock).toHaveBeenCalledWith({
             from: process.env.SMTP_MAIL,
@@ -63,13 +82,18 @@ describe('EMAIL MOCKS.', async () => {
             subject: `Your Dream Note OTP for password-reset.`,
             text: `Your one time passcode (OTP) is 123456. This will expire in 10 minutes.`            
         })    
-    })
 
-    test('Email is not sent if provided address is not associated with a user.', async () => {
-        const sendMailMock = nodemailer.createTransport().sendMail;
-        // Use unsaved address - sendMailMock doesnt get called
-        await request(server).post(url).send({email: 'example@email.com'}).expect(200)
-        expect(sendMailMock).toHaveBeenCalledTimes(0);
+        // Otp should now be saved in db
+        const otpRecords = await Otp.find({
+            userId: userOneId,
+            email: userOneCreds.email,
+            purpose: 'password-reset',
+            used: false
+        })
+        expect(otpRecords).toHaveLength(1)
+        const hashedOtp = otpRecords[0].otp
+        const isMatch = await bcrypt.compare('123456', hashedOtp)
+        expect(isMatch).toBe(true)
     })
 })
 
