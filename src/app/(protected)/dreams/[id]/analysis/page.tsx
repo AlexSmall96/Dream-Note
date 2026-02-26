e464ecf75510ddfdba675a2e9cf022b2863ed169
Alex Small <alex.small739@gmail.com>
2026-02-26 19:55:57 +0000

    Show full text and description for main analysis and toggle select on click

--- src/app/(protected)/dreams/[id]/analysis/page.tsx
+++ src/app/(protected)/dreams/[id]/analysis/page.tsx
@@ -1,18 +1,23 @@
 "use client";
 import { useEffect, useState } from "react";
 import { SavedAnalysis } from "@/types/aiAnalysis";
-import { fetchSavedAnalyses, toggleFavoriteAnalysis } from "@/lib/api/aiAnalysis";
+import { deleteAnalysis, fetchSavedAnalyses, toggleFavoriteAnalysis } from "@/lib/api/aiAnalysis";
 import Analysis from "@/components/dreams/Analysis";
+import { useRouter } from "next/navigation";
 
 export default function DreamAnalysisPage ({ params }: { params: { id: string } }) {
 
     const [analyses, setAnalyses] = useState<SavedAnalysis[]>([])
+    const [mainAnalysis, setMainAnalysis] = useState<SavedAnalysis | null>(null)
+
+    const router = useRouter()
     
     useEffect(() => {
         const getSavedAnalyses = async () => {
             try {
                 const response = await fetchSavedAnalyses(params.id)
                 setAnalyses(response.analyses)
+                setMainAnalysis(response.analyses[0])
             } catch (err){
                 console.log(err)
             }
@@ -30,20 +35,43 @@ export default function DreamAnalysisPage ({ params }: { params: { id: string } 
         }
     }
 
+    const handleDelete = async (analysisId: string) => {
+        try {
+            await deleteAnalysis(params.id, analysisId)
+        } catch (err) {
+            console.log(err)
+        }
+        setAnalyses(prev => prev.filter(a => a._id !== analysisId))
+    }
+
+    const viewFullAnalysis = (_id: string) => {
+        setMainAnalysis(analyses.filter(a => a._id === _id)[0])
+    }
+    
     return (
-        <div className="flex flex-col items-center m-4">
+        <>
             <h1>Previous Analysis</h1>
-            {analyses.length > 0 && analyses.map(
-                analysis => 
-                    <Analysis
-                        key={analysis._id}
-                        text={analysis.text} 
-                        createdAt={analysis.createdAt.toString()} 
-                        modelUsed={analysis.modelUsed} 
-                        isFavourite={analysis.isFavorite}
-                        onClickHeart={() => toggleFavorite(analysis._id)} 
-                    />
-            )}
-        </div>
+            <div className='grid grid-cols-2 gap-4'>
+                <div>
+                    {analyses.length > 0 && analyses.map(
+                        analysis => 
+                            <Analysis
+                                analysisData={analysis}
+                                onClickHeart={() => toggleFavorite(analysis._id)}
+                                onClickText={() => viewFullAnalysis(analysis._id)}
+                                onDelete={() => handleDelete(analysis._id)} 
+                                selected={mainAnalysis?._id === analysis._id}
+                            />
+                    )}
+                    <button onClick={() => router.replace(`/dreams/${params.id}`)} className='btn btn-gray bg-gray-300 p-1 m-2'>Back to Dream</button>
+                </div>
+                <div>
+                    <p className='font-bold'>Analysis:</p>
+                    <p className='italic'>{mainAnalysis?.text}</p>
+                    <p className='font-semibold'>Description used:</p>
+                    <p className="text-gray-500 text-sm flex gap-4 mt-1">{mainAnalysis?.descriptionSnapshot}</p>
+                </div>
+            </div>
+        </>
     )
 }
