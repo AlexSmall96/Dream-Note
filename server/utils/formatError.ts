b3c7ad7a0a3d726ac9e6a19270d87055c7922839
Alex Small <alex.small739@gmail.com>
2026-02-26 09:05:49 +0000

    Remove formatError util

--- server/utils/formatError.ts
+++
@@ -1,7 +0,0 @@
-export const formatError = (msg: string, param?: string): {errors: {msg: string, param?: string}[]} => {
-    return {
-        errors: [
-            {msg, param}
-        ]
-    }
-}
