3b8aba12dc338447aa3bc3e700e2342e61e90c35
Alex Small <alex.small739@gmail.com>
2026-02-26 19:58:31 +0000

    Add DeleteModal and onClickText to Analysis component

--- src/components/dreams/Analysis.tsx
+++ src/components/dreams/Analysis.tsx
@@ -1,30 +1,37 @@
 import { format } from "date-fns"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as solidHeart, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
+import { DeleteModal } from "@/components/ui/DeleteModal";
+import { SavedAnalysis } from "@/types/aiAnalysis";
 
 export default function Analysis ({
-    text, createdAt, modelUsed, isFavourite, onClickHeart
+    analysisData, onClickHeart, onClickText, onDelete, selected
 }:{
-    text: string,
-    createdAt: string,
-    modelUsed: string,
-    isFavourite: boolean,
-    onClickHeart: () => Promise<void>
+    analysisData: SavedAnalysis,
+    onClickHeart: () => Promise<void>,
+    onClickText: () => void,
+    onDelete: () => Promise<void>
+    selected: boolean
 }) {
-    const formatted = format(new Date(createdAt), "PP")
+    const {tone, style, text, isFavorite, createdAt } = analysisData
+    const formattedDate = format(new Date(createdAt), "PP")
 
     return (
-        <div className="flex items-center p-4 border-b border-gray-200 hover:bg-gray-50 transition">
+        <div className={`flex items-center p-4 border-b ${selected? 'bg-gray-200': ''} border-gray-200`}>
             <div className="flex-1 ml-2">
-                <div className="text-gray-800 font-medium truncate max-w-xs">{text}</div>
+                <div onClick={onClickText} className="text-gray-800 font-medium hover:underline cursor-pointer truncate max-w-xs">{text}</div>
                 <div className="text-gray-500 text-sm flex gap-4 mt-1">
-                <span>Created: {formatted}</span>
-                <span>Model: {modelUsed}</span>
+                <span>Created: {formattedDate}</span>
+                <span>{`${tone} | ${style}`}</span>
                 <FontAwesomeIcon
-                    icon={isFavourite ? solidHeart : regularHeart}
+                    icon={isFavorite ? solidHeart : regularHeart}
                     onClick={onClickHeart}
                     className='cursor-pointer transition'
+                />
+                <DeleteModal 
+                    handleDelete={onDelete}
+                    message='Are you sure you want to delete this analysis?'
                 />
                 </div>
             </div>
