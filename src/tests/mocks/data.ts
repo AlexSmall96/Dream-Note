e48412a559797d637de58f49bc8cf6f646d13189
Alex Small <alex.small739@gmail.com>
2026-03-04 18:48:07 +0000

    Add get dream stats url to handlers

--- src/tests/mocks/data.ts
+++ src/tests/mocks/data.ts
@@ -1,13 +1,15 @@
+import { last6monthsDreams } from "@/types/dreams";
 import { ThemeMonthCount } from "@/types/themes";
-
+import { MonthLabel } from "@/lib/filters/dateRanges";
 
 const now = new Date()
-const data: ThemeMonthCount[] = []
+const themeData: ThemeMonthCount[] = []
+const dreamCounts: last6monthsDreams = []
 
 const themes = ['Fear', 'Nature', 'Adventure', 'Animals', 'Freedom']
-const monthLabels: string[] = []
+const monthLabels: MonthLabel[] = []
 
-// Generate mock data for ThemeBarChart
+// ThemeBarChart data
 for (let i=1; i<=6; i++){
     const date = new Date(now.getFullYear(), now.getMonth() - i, 27)
     const themeCounts = themes.reduce<Record<string, number>>((acc, curr, index) => {
@@ -15,13 +17,22 @@ for (let i=1; i<=6; i++){
         return acc
     }, {})
 
-    const monthLabel = date.toLocaleString('en-US', { month: 'short' })
-    data.push({
+    const monthLabel = date.toLocaleString('en-US', { month: 'short' }) as MonthLabel
+    themeData.push({
         month: monthLabel, ...themeCounts
     })
-
     monthLabels.push(monthLabel)
-
 }
 
-export {themes, data, monthLabels}
+// DreamsLineChart data
+for (let i=1; i<=6; i++){
+    const year = now.getFullYear()
+    const date = new Date(year, now.getMonth() - i, 27)
+    const monthLabel = date.toLocaleString('en-US', { month: 'short' }) as MonthLabel
+
+    dreamCounts.push({
+        year, month: date.getMonth(), label: monthLabel, dreams: 2 * i + 1
+    })
+}   
+
+export {themes, themeData, dreamCounts, monthLabels}
--- src/tests/mocks/handlers.ts
+++ src/tests/mocks/handlers.ts
@@ -1,6 +1,6 @@
 import { http, HttpResponse } from "msw";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-import {themes, data} from '@/tests/mocks/data'
+import { themes, themeData, dreamCounts } from '@/tests/mocks/data'
 
 if (!API_URL) {
   	throw new Error("PUBLIC_API_URL is not defined");
@@ -8,12 +8,18 @@ if (!API_URL) {
 
 
 export const themeStatsUrl = `${API_URL}/themes/chart-stats`
+export const dreamStatsUrl = `${API_URL}/dreams/chart-stats`
 
 export const handlers = [
     // Dashboard charts data
     http.get(themeStatsUrl, () => {
         return HttpResponse.json({
-            themes, data
+            themes, data: themeData
         }, {status: 200})
     }),
+    http.get(dreamStatsUrl, () => {
+        return HttpResponse.json({
+            dreamCounts
+        }, {status: 200})        
+    })
 ]
