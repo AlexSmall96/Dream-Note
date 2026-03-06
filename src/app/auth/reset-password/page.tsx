6c6b26e25e6096027391a02c1c6aaac9b6e2ab6a
Alex Small <alex.small739@gmail.com>
2026-03-06 19:31:49 +0000

    Use variable button text in update email page and reset password page

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -7,6 +7,8 @@ export default function RequestPasswordReset () {
     return (
         <div className="flex flex-col items-center m-4">
             <EmailForm<{otp: string, email: string}> 
+                emailPlaceholder='Enter your email address'
+                emailButtonText='Send OTP to recover your password'
                 requestFn={requestPasswordReset} 
                 verifyFn={verifyResetOTP} 
                 buildVerifyPayload={(otp, email) => ({otp, email})} 
