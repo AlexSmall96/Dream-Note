1627a453cf2a7f4fb5c0d2565ae95e74a1501c96
Alex Small <alex.small739@gmail.com>
2026-03-06 19:41:29 +0000

    Remove login.test file descibe blocks for clarity

--- server/tests/users/auth/login.test.ts
+++ server/tests/users/auth/login.test.ts
@@ -44,63 +44,61 @@ const guestTitles = guestData.map(d => d.dream.title)
 // Define url
 const loginUrl = baseUrl + '/login'
 
-describe('LOGIN/LOGOUT SUCCESS', () => {
-    test('Login to non guest account should be successful with correct credentials, and logout should succeed using generated token.', async () => {
-        // Extract user id
-        const id = userOne._id.toString()
-        // Assert that only no tokens currently exist in database
-        let user = await User.findByIdOrThrowError(id)
-        expect(user.tokens).toHaveLength(0)
-        // Send correct data
-        const response = await request(server).post(loginUrl).send(userOne).expect(200)
-        // User object and token should be returned
-        expect(response.body.user).toMatchObject({email:userOne.email, _id: id})
-        const token = response.body.token
-        expect(token).not.toBeNull()
-        expect(response.body).not.toHaveProperty('password')
-        // Assert not guest account
-        expect(response.body.isGuest).toBe(false)
-        // Assert token was added to User in database
-        user = await User.findByIdOrThrowError(id)
-        expect(user.tokens).toHaveLength(1)
-        // Logout
-        await request(server).post(`${baseUrl}/logout`).set('Authorization', `Bearer ${token}`).expect(200)
-        // Assert database has changed - Token should have been removed
-        user = await User.findByIdOrThrowError(id)
-        expect(user.tokens).toHaveLength(0)        
-    })
+test('Login to non guest account should be successful with correct credentials, and logout should succeed using generated token.', async () => {
+    // Extract user id
+    const id = userOne._id.toString()
+    // Assert that only no tokens currently exist in database
+    let user = await User.findByIdOrThrowError(id)
+    expect(user.tokens).toHaveLength(0)
+    // Send correct data
+    const response = await request(server).post(loginUrl).send(userOne).expect(200)
+    // User object and token should be returned
+    expect(response.body.user).toMatchObject({email:userOne.email, _id: id})
+    const token = response.body.token
+    expect(token).not.toBeNull()
+    expect(response.body).not.toHaveProperty('password')
+    // Assert not guest account
+    expect(response.body.isGuest).toBe(false)
+    // Assert token was added to User in database
+    user = await User.findByIdOrThrowError(id)
+    expect(user.tokens).toHaveLength(1)
+    // Logout
+    await request(server).post(`${baseUrl}/logout`).set('Authorization', `Bearer ${token}`).expect(200)
+    // Assert database has changed - Token should have been removed
+    user = await User.findByIdOrThrowError(id)
+    expect(user.tokens).toHaveLength(0)        
+})
 
-    test('Login to guest account should be successful and seed data should be reset.', async () => {
-        // Assert that new guest dream and theme are currently saved in db
-        const newDream = Dream.findById(newGuestDreamId)
-        const newTheme = Theme.findById(newGuestThemeId)
-        expect(newDream).not.toBeNull()
-        expect(newTheme).not.toBeNull()
-        // Login as guest
-        const response = await request(server).post(baseUrl + '/login-guest').send({}).expect(200)
-        expect(response.body.isGuest).toBe(true)
+test('Login to guest account should be successful and seed data should be reset.', async () => {
+    // Assert that new guest dream and theme are currently saved in db
+    const newDream = Dream.findById(newGuestDreamId)
+    const newTheme = Theme.findById(newGuestThemeId)
+    expect(newDream).not.toBeNull()
+    expect(newTheme).not.toBeNull()
+    // Login as guest
+    const response = await request(server).post(baseUrl + '/login-guest').send({}).expect(200)
+    expect(response.body.isGuest).toBe(true)
 
-        // Correct seed dream and theme data is created
-        const guestsDreams: DreamInterface[] = await Dream.find({owner: guestUser._id})
-        expect(guestsDreams).toHaveLength(4)
-        await Promise.all(
-            guestTitles.map(async (title) => {
-                const dream = await Dream.findOne({title}) as DreamDocument
-                const dreamData = titleToDream(title)
-                const {description, notes, date} = dream
-                expect({description, notes, date, title}).toMatchObject(dreamData.dream)
-                expect(dream.owner.toString()).toBe(guestUser._id.toString())
-                // Themes associated with dream should be correct
-                const themes = await Theme.find({dream: dream._id})
-                const themeNames = themes.map(t => t.theme)
-                expect(themeNames.sort()).toEqual(dreamData.themes.sort())
-            })
-        )
+    // Correct seed dream and theme data is created
+    const guestsDreams: DreamInterface[] = await Dream.find({owner: guestUser._id})
+    expect(guestsDreams).toHaveLength(4)
+    await Promise.all(
+        guestTitles.map(async (title) => {
+            const dream = await Dream.findOne({title}) as DreamDocument
+            const dreamData = titleToDream(title)
+            const {description, notes, date} = dream
+            expect({description, notes, date, title}).toMatchObject(dreamData.dream)
+            expect(dream.owner.toString()).toBe(guestUser._id.toString())
+            // Themes associated with dream should be correct
+            const themes = await Theme.find({dream: dream._id})
+            const themeNames = themes.map(t => t.theme)
+            expect(themeNames.sort()).toEqual(dreamData.themes.sort())
+        })
+    )
 
-        // New dream and theme should have been deleted
-        const nullDream = await Dream.findById(newGuestDreamId)
-        const nullTheme = await Dream.findById(newGuestThemeId)
-        expect(nullDream).toBeNull()
-        expect(nullTheme).toBeNull()
-    })
+    // New dream and theme should have been deleted
+    const nullDream = await Dream.findById(newGuestDreamId)
+    const nullTheme = await Dream.findById(newGuestThemeId)
+    expect(nullDream).toBeNull()
+    expect(nullTheme).toBeNull()
 })
