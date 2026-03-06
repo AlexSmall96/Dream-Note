f9ebb46ce5fc1a3aa33f847d41a0511b022aa059
Alex Small <alex.small739@gmail.com>
2026-03-06 20:30:36 +0000

    Move mongoose connection into new setupBackend test file

--- test/setupEnv.ts
+++ test/setupEnv.ts
@@ -1,18 +1,2 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './test.env' });
-import { afterAll, beforeAll } from 'vitest'
-import mongoose from 'mongoose'
-import { connectToDB } from '../server/utils/connectToDB.js'
-
-beforeAll(async () => {
-    if (process.env.DATABASE_URL){
-        await connectToDB(true)
-    }
-})
-
-
-afterAll(async () => {
-    if (mongoose?.connection?.readyState !== 0) {
-        await mongoose.disconnect()
-    }
-})
