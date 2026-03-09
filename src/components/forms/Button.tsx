5eb04abb4813bd45a5efeede223930c63fb6126c
Alex Small <alex.small739@gmail.com>
2026-03-09 13:08:29 +0000

    Move Input and Button components into forms folder

--- src/components/forms/Button.tsx
+++ src/components/forms/Button.tsx
@@ -1,16 +1,23 @@
-export default function ({
+import { MouseEventHandler } from "react"
+
+export default function Button({
     text,
-    disabled
+    disabled,
+    type = 'submit',
+    onClick = () => {}
 }:{
     text: string,
     disabled: boolean
+    type?: 'submit' | 'button'
+    onClick?: MouseEventHandler<HTMLButtonElement>
 }) {
     const colorClass = disabled? 'bg-gray-400' : 'bg-blue-500 hover:bg-blue-700'
 
     return (
         <button 
-            type='submit' 
+            type={type}
             className={`rounded-lg ${colorClass} text-white font-bold p-2`} disabled={disabled}
+            onClick={onClick}
         >
                 {text}
         </button>
