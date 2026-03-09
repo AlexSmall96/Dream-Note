9819c43c93ade6c20dae8b97a31fb350725f9472
Alex Small <alex.small739@gmail.com>
2026-03-09 18:26:53 +0000

    Remove conditional href from LinkWithMessage component

--- src/components/forms/LinkWithMessage.tsx
+++ src/components/forms/LinkWithMessage.tsx
@@ -1,20 +1,18 @@
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 export default function LinkWithMessage ({msg, linkText, href, disabled = false}:{
     msg: string, 
     linkText: string,
     href: string,
-    disabled: boolean
+    disabled?: boolean
 }) {
-    const pathname = usePathname()
 
     return (
         <div className="text-center">
             <span className="text-gray-500">{msg} </span>
             <Link 
-                href={disabled ? pathname : href}
-                className={disabled? 'text-gray-500 pointer-events-none' :"hover:underline text-blue-500"}>
+                href={href}
+                className={disabled? 'text-gray-500 pointer-events-none' : "hover:underline text-blue-500"}>
                 {linkText}
             </Link>
         </div>
