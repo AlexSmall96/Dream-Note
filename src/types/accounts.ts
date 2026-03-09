6aeee8efedf28070cb150de33c66ec8208cb4823
Alex Small <alex.small739@gmail.com>
2026-03-09 19:36:06 +0000

    Remove accounError, replaced with errorType in responses.ts

--- src/types/accounts.ts
+++ src/types/accounts.ts
@@ -3,14 +3,6 @@ export type passwordUpdateInput = {
     password?: string
 }
 
-export type accountError = {
-    error: string
-}
-
-export type accountErrorArray = {
-    errors: {param: string, msg: string, value: string}[]
-}
-
 export type accountMessage = {
     message: string
 }
