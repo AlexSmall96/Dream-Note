5b4c341f1f0f69151fde3188841a54e70c878bf0
Alex Small <alex.small739@gmail.com>
2026-03-09 18:26:14 +0000

    Update error type to string and add disable state to buttons

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { login, loginGuest } from '@/lib/api/auth'
 import LinkWithMessage from "../forms/LinkWithMessage"
 import { Input } from "../forms/Input"
@@ -9,14 +9,18 @@ import { Card } from "../ui/Card"
 
 export default function LoginForm() {
     const [formData, setFormData] = useState({email: '', password: ''})
-    const [error, setError] = useState({param: '', msg: ''})
-    
+    const [error, setError] = useState<string | null>(null)
+    const [disabled, setDisabled] = useState(false)
+    const [waiting, setWaiting] = useState(false)
+
     async function handleSubmit(e: React.FormEvent) {
         e.preventDefault()
+        setWaiting(true)
         const {email, password} = formData
         const result = await login({ email, password })
         if ('errors' in result){
-           return setError(result.errors[0])
+           setWaiting(false)
+           return setError(result.errors[0].msg)
         }   
         window.location.href = "/dreams"
     }
@@ -30,9 +34,20 @@ export default function LoginForm() {
         }
     }
 
+    useEffect(() => {
+        const {email, password} = formData
+        const emptyInput = email === '' || password === ''
+        if (waiting || emptyInput || error){
+            setDisabled(true)
+        } else {
+            setDisabled(false)
+        }
+    }, [waiting, error, formData])
+
+    
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target
-        setError({param: '', msg: ''}) // Clear error message if user inputs new data
+        setError(null) // Clear error message if user inputs new data
         setFormData(prevData => ({ ...prevData, [name]: value }))
     }
     
@@ -44,20 +59,23 @@ export default function LoginForm() {
                     onChange={handleChange}
                     name="email"
                     placeholder="Email"
+                    aria-label='email'
+                    disabled={waiting}
                 />
-                {error.param === 'email' ? error.msg : ''}
                 <Input 
                     type="password"
                     value={formData.password}
                     onChange={handleChange}
                     name="password"
-                    placeholder="Password"                
+                    placeholder="Password"   
+                    aria-label='password'  
+                    disabled={waiting}           
                 />
-                {error.param === 'password' ? error.msg : ''}
-                <Button text='Login' disabled={false} />
-                <Button text='Continue as Guest' disabled={false} onClick={handleLoginGuest}/>
-                <LinkWithMessage msg="Don't have an account?" href='/auth/signup' linkText="Signup" disabled={false} />
-                <LinkWithMessage msg="Forgotten password?" href='/auth/reset-password' linkText="Reset Password" disabled={false} />
+                {error  && <p role='alert'>{error}</p>}
+                <Button text={waiting? 'Logging in...' : 'Login'} disabled={disabled} />
+                <Button text='Continue as Guest' disabled={waiting} onClick={handleLoginGuest}/>
+                <LinkWithMessage msg="Don't have an account?" href='/auth/signup' linkText="Sign up" disabled={waiting} />
+                <LinkWithMessage msg="Forgotten password?" href='/auth/reset-password' linkText="Reset Password" disabled={waiting} />
             </form>
         </Card>
     )
