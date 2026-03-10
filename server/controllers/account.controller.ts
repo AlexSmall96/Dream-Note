da8d2152b7f8ba7b8b57bbe83abe271a2b09f43c
Alex Small <alex.small739@gmail.com>
2026-03-10 09:41:11 +0000

    Move reset password functions from account to auth

--- server/controllers/account.controller.ts
+++ server/controllers/account.controller.ts
@@ -25,18 +25,6 @@ export class AccountController {
         }
     }
 
-    // Send otp to existing email address 
-    public requestPasswordReset = async (req: Request, res: Response, next: NextFunction) => {
-        const email = req.body.email
-        try {
-            await this.accountService.requestPasswordReset(email)
-            // Always return success message to prevent email enumeration, even if email doesn't exist or error occurs when sending OTP
-            return res.json({ message: "If an account is associated with the provided email address, a OTP will be sent." })            
-        } catch (err){
-            next(err)
-        }
-    }
-
     public requestEmailVerification = async (req: Request, res: Response, next: NextFunction) => {
         const authReq = req as AuthenticatedRequest
         const email = authReq.user.email
@@ -72,30 +60,6 @@ export class AccountController {
         } catch (err){
             next(err)
         }        
-    }
-
-    // Verify otp for password reset, requires email that otp was sent to and otp value
-    public verifyResetOtp = async (req: Request, res:Response, next: NextFunction) => {
-        const otp = req.body.otp
-        const email = req.body.email
-        try {
-            const resetToken = await this.accountService.verifyResetOtpAndSendToken(otp, email)
-            res.json({ resetToken })
-        } catch (err) {
-            next(err)
-        }
-    }
-
-    // Reset password for unauthenticated user using token from verified otp
-    public resetPassword = async (req: Request, res:Response, next: NextFunction) => {
-        const resetToken = req.body.resetToken
-        const newPassword = req.body.password
-        try {
-            await this.accountService.resetPassword(resetToken, newPassword)
-            res.json({ message: "Password updated successfully." })
-        } catch (err){
-            next(err)
-        }
     }
 
     public updatePassword = async (req: Request, res:Response, next: NextFunction) => {
--- server/services/users/auth.service.ts
+++ server/services/users/auth.service.ts
@@ -5,12 +5,19 @@ import { User } from "../../models/user.model.js";
 import { UserInterface, UserDocument } from "../../interfaces/user.interfaces.js";
 import { guestData } from '../../seed-data/guestSeedData.js'
 import { AccountService } from "./account.service.js";
+import { OtpService } from "./otp.service.js";
+import { ResetTokenService } from "./reset-token.service.js";
+import { AppError } from "../../utils/appError.js";
+import { EmailService } from "./email.service.js";
 
 @injectable()
 export class AuthService {
 
     constructor(
         @inject(AccountService) private accountService: AccountService,
+        @inject(OtpService) private otpService: OtpService,
+        @inject(ResetTokenService) private resetTokenService: ResetTokenService,
+        @inject(EmailService) private emailService: EmailService,
     ){}
 
     // Sign up
@@ -33,6 +40,39 @@ export class AuthService {
         await user.save()
     }
 
+    public async requestPasswordReset(email: string){
+        const otp = await this.otpService.generateOTP(email, 'password-reset')
+        if (otp){
+            await this.emailService.sendMailWithData('password-reset', email, otp, 10)
+        }
+    }
+
+        public async resetPassword(resetToken: string, newPassword: string){
+        const payload = this.resetTokenService.verifyPasswordResetToken(resetToken)
+        const { userId, otpId } = payload
+        const user = await User.findById(userId)
+        if (!user) {
+            throw new AppError('Invalid reset session.', 400, 'resetToken')
+        }
+        // Find otp
+        const otp = await this.otpService.findUsedOtp(otpId, userId, 'password-reset')
+        if (!otp){
+            throw new AppError('Invalid reset session.', 400, 'resetToken')
+        }
+        // Delete otp as password update is successful
+        await this.accountService.updatePassword(newPassword, userId)
+        await this.otpService.deleteOtp(otpId)
+    }
+
+    public async verifyResetOtpAndSendToken(otp: string, email: string){
+        // Find otp and mark as used - dont delete yet as needed to verify reset token
+        const otpRecord = await this.otpService.findOtpByEmailAndUpdate(otp, email, 'password-reset')
+        if (!otpRecord){
+            throw new AppError('Invalid or expired OTP.', 400, 'otp')
+        }
+        const resetToken = this.resetTokenService.generatePasswordResetToken(otpRecord.userId.toString(), otpRecord._id.toString())
+        return resetToken
+    }
     // Save dreams and themes when guest logs in
     public async seedGuestData(guestId: string){
         await Promise.all(guestData.map(async (data) => {
