bd33ed6e6fc76783c85b75dd5942861363eb8e1d
Alex Small <alex.small739@gmail.com>
2026-03-10 09:59:50 +0000

    Rename env files and add to gitignore, move frontend and backend into correct folders

---
+++ server/tests/setup/setupEnv.ts
@@ -0,0 +1,2 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './server/test.backend.env', quiet: true });
---
+++ src/tests/setup/setupEnv.ts
@@ -0,0 +1,2 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './src/test.frontend.env', quiet: true });
