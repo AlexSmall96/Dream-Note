b84efb86878888bb3efd9e80d38d961d623cc47b
Alex Small <alex.small739@gmail.com>
2026-03-11 07:40:11 +0000

    Add optioanl fontawesome icons prop to Button and LinkWithMessage

--- src/components/forms/Button.tsx
+++ src/components/forms/Button.tsx
@@ -1,17 +1,23 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core"
 import { MouseEventHandler } from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function Button({
     text,
     disabled,
     type = 'submit',
-    onClick = () => {}
+    onClick = () => {},
+    danger = false,
+    icon
 }:{
     text: string,
     disabled: boolean
     type?: 'submit' | 'button'
     onClick?: MouseEventHandler<HTMLButtonElement>
+    danger?: boolean,
+    icon?: IconProp
 }) {
-    const colorClass = disabled? 'bg-gray-400' : 'bg-blue-500 hover:bg-blue-700'
+    const colorClass = disabled? 'bg-gray-400' : danger ? 'bg-orange-500 hover:bg-orange-700' : 'bg-blue-500 hover:bg-blue-700'
 
     return (
         <button 
@@ -19,7 +25,7 @@ export default function Button({
             className={`rounded-lg ${colorClass} text-white font-bold p-2`} disabled={disabled}
             onClick={onClick}
         >
-                {text}
+                {icon && <FontAwesomeIcon icon={icon} />} { text}
         </button>
     )
 }
--- src/components/forms/LinkWithMessage.tsx
+++ src/components/forms/LinkWithMessage.tsx
@@ -1,10 +1,14 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
-export default function LinkWithMessage ({msg, linkText, href, disabled = false}:{
-    msg: string, 
+export default function LinkWithMessage ({msg, linkText, icon, href, disabled = false, danger = false}:{
+    msg?: string, 
     linkText: string,
+    icon?: IconProp,
     href: string,
-    disabled?: boolean
+    disabled?: boolean,
+    danger? : boolean
 }) {
 
     return (
@@ -12,8 +16,9 @@ export default function LinkWithMessage ({msg, linkText, href, disabled = false}
             <span className="text-gray-500">{msg} </span>
             <Link 
                 href={href}
-                className={disabled? 'text-gray-500 pointer-events-none' : "hover:underline text-blue-500"}>
-                {linkText}
+                className={disabled? 'text-gray-500 pointer-events-none' : `hover:underline ${danger? 'text-orange-500' :'text-blue-500'}`}>
+                    {icon && <FontAwesomeIcon icon={icon} />}
+                    <span >{linkText}</span>
             </Link>
         </div>
     )
