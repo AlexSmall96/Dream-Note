a8fb33949c2c64933ef8b1200de57e168f2a800c
Alex Small <alex.small739@gmail.com>
2026-03-11 15:36:00 +0000

    Update reset password page as a server component using redirect

--- src/app/auth/reset-password/new/page.tsx
+++ src/app/auth/reset-password/new/page.tsx
@@ -1,41 +1,22 @@
-'use client'
-import ResetPasswordForm from "@/components/auth/ResetPasswordForm"
-import { useSearchParams } from 'next/navigation'
-import { useState, useEffect } from "react"
-import { useRouter } from "next/navigation";
-import { verifyResetToken } from "@/lib/api/auth";
+import ResetPasswordForm from '@/components/auth/ResetPasswordForm'
+import { redirect } from 'next/navigation'
+import { verifyResetToken } from '@/lib/api/auth'
 
-export default function NewPasswordPage (){
-    const searchParams = useSearchParams()
-    const [loading, setLoading] = useState(true)
-    const token = searchParams.get('token')
-    const router = useRouter();
+export default async function NewPasswordPage({
+    searchParams,
+}: {
+    searchParams: { token?: string }
+}) {
+    const token = searchParams.token
 
-    useEffect(() => {
-        const checkTokenIsValid = async () => {
-            try {
-                if (!token){
-                    throw null
-                }
-                const result = await verifyResetToken(token)
-                if ('errors' in result){
-                    throw null
-                }
-                    setLoading(false)
-            } catch (err){
-                // Throw null errors above to trigger catch block and redirect to request reset page if token is invalid
-                return router.push('/auth/reset-password')
-            }
-        }
-        checkTokenIsValid()
-    }, [token, router])
+    if (!token) {
+        redirect('/auth/reset-password')
+    }
 
-    if (loading){
-        return (
-            <div className="flex flex-col items-center m-4">
-                Loading...
-            </div>
-        )
+    const result = await verifyResetToken(token)
+
+    if ('errors' in result) {
+        redirect('/auth/reset-password')
     }
 
     return (
@@ -43,7 +24,7 @@ export default function NewPasswordPage (){
             <h1 className="m-2">
                 One time passcode correct. Enter your new password below.
             </h1>
-                <ResetPasswordForm token={token!} />
+            <ResetPasswordForm token={token} />
         </div>
     )
 }
