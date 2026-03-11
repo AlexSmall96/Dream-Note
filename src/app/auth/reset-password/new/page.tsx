0218c283109ca427e685ebfb2ef21d238f7fa085
Alex Small <alex.small739@gmail.com>
2026-03-11 15:14:12 +0000

    Check token in reset password page, redirect if invalid

--- src/app/auth/reset-password/new/page.tsx
+++ src/app/auth/reset-password/new/page.tsx
@@ -1,16 +1,49 @@
 'use client'
 import ResetPasswordForm from "@/components/auth/ResetPasswordForm"
-import { Suspense } from 'react'
+import { useSearchParams } from 'next/navigation'
+import { useState, useEffect } from "react"
+import { useRouter } from "next/navigation";
+import { verifyResetToken } from "@/lib/api/auth";
 
 export default function NewPasswordPage (){
+    const searchParams = useSearchParams()
+    const [loading, setLoading] = useState(true)
+    const token = searchParams.get('token')
+    const router = useRouter();
+
+    useEffect(() => {
+        const checkTokenIsValid = async () => {
+            try {
+                if (!token){
+                    throw null
+                }
+                const result = await verifyResetToken(token)
+                if ('errors' in result){
+                    throw null
+                }
+                    setLoading(false)
+            } catch (err){
+                // Throw null errors above to trigger catch block and redirect to request reset page if token is invalid
+                return router.push('/auth/reset-password')
+            }
+        }
+        checkTokenIsValid()
+    }, [token, router])
+
+    if (loading){
+        return (
+            <div className="flex flex-col items-center m-4">
+                Loading...
+            </div>
+        )
+    }
+
     return (
         <div className="flex flex-col items-center m-4">
             <h1 className="m-2">
                 One time passcode correct. Enter your new password below.
             </h1>
-            <Suspense fallback={<div>Loading form...</div>}>
-                <ResetPasswordForm />
-            </Suspense>
+                <ResetPasswordForm token={token!} />
         </div>
     )
 }
