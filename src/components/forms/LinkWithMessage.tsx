92724fb85cc96d0ad1727f68e065ee8ca5a3586d
Alex Small <alex.small739@gmail.com>
2026-03-12 12:19:31 +0000

    Update LinkWithMessage component to allow cusom text color and pulse icon animation

--- src/components/forms/LinkWithMessage.tsx
+++ src/components/forms/LinkWithMessage.tsx
@@ -2,13 +2,14 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
-export default function LinkWithMessage ({msg, linkText, icon, href, disabled = false, danger = false}:{
+export default function LinkWithMessage ({msg, linkText, icon, href, disabled = false, danger = false, textColor = 'text-blue-500'}:{
     msg?: string, 
-    linkText: string,
+    linkText?: string,
     icon?: IconProp,
     href: string,
     disabled?: boolean,
-    danger? : boolean
+    danger? : boolean,
+    textColor?: string
 }) {
 
     return (
@@ -16,8 +17,8 @@ export default function LinkWithMessage ({msg, linkText, icon, href, disabled = 
             <span className="text-gray-500">{msg} </span>
             <Link 
                 href={href}
-                className={disabled? 'text-gray-500 pointer-events-none' : `hover:underline ${danger? 'text-orange-500' :'text-blue-500'}`}>
-                    {icon && <FontAwesomeIcon icon={icon} />}
+                className={disabled? 'text-gray-500 pointer-events-none' : `hover:underline ${danger? 'text-orange-500' : textColor}`}>
+                    {icon && <FontAwesomeIcon icon={icon} className='hover:animate-pulse' />}
                     <span >{linkText}</span>
             </Link>
         </div>
