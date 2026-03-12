9c5448891b66e916585e2039dc5b2e5af0162d31
Alex Small <alex.small739@gmail.com>
2026-03-12 16:11:05 +0000

    Add extraClass property to LinkWithMessage

--- src/components/forms/LinkWithMessage.tsx
+++ src/components/forms/LinkWithMessage.tsx
@@ -2,14 +2,24 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
-export default function LinkWithMessage ({msg, linkText, icon, href, disabled = false, danger = false, textColor = 'text-blue-500'}:{
+export default function LinkWithMessage ({
+    msg, 
+    linkText, 
+    icon, 
+    href, 
+    disabled = false, 
+    danger = false, 
+    textColor = 'text-blue-500', 
+    extraClass
+}:{
     msg?: string, 
     linkText?: string,
     icon?: IconProp,
     href: string,
     disabled?: boolean,
     danger? : boolean,
-    textColor?: string
+    textColor?: string,
+    extraClass? : string
 }) {
 
     return (
@@ -17,7 +27,7 @@ export default function LinkWithMessage ({msg, linkText, icon, href, disabled = 
             <span className="text-gray-500">{msg} </span>
             <Link 
                 href={href}
-                className={disabled? 'text-gray-500 pointer-events-none' : `hover:underline ${danger? 'text-orange-500' : textColor}`}>
+                className={disabled? 'text-gray-500 pointer-events-none' : `hover:underline ${danger? 'text-orange-500' : textColor} ${extraClass}`}>
                     {icon && <FontAwesomeIcon icon={icon} className='hover:animate-pulse' />}
                     <span >{linkText}</span>
             </Link>
