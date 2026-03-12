404466922b6d4fac30ed32a030d0e5555a9d2739
Alex Small <alex.small739@gmail.com>
2026-03-12 12:20:09 +0000

    Create DreamCard component to use in DreamView for improved readability

---
+++ src/components/dreams/DreamCard.tsx
@@ -0,0 +1,53 @@
+import { useDreamView } from "@/contexts/DreamViewContext"
+import LinkWithMessage from "../forms/LinkWithMessage";
+import { faFeatherPointed as faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan as faDelete } from "@fortawesome/free-solid-svg-icons";
+import { useParams } from "next/navigation"
+
+export default function DreamCard () {
+    const {dream, themes } = useDreamView()
+    const params = useParams()
+    const id = params.id
+
+    return (
+        <div className="mt-8 mb-8 p-8 bg-gradient-to-br from-purple-50 to-blue-50 rounded-lg shadow-lg border border-purple-100 w-full max-w-xl">
+            <div className="flex justify-between items-start mb-4">
+                <div>
+                    <h2 className="text-lg text-gray-700 italic">
+                    {new Date(dream.date).toLocaleDateString()}
+                    </h2>
+                    <h1 className="text-2xl font-serif text-gray-900 leading-relaxed">
+                        {dream.title}
+                    </h1>
+                </div>
+        
+                <div className="flex gap-3">
+                    <LinkWithMessage
+                        href={`/dreams/${id}/edit`}
+                        icon={faEdit}
+                        textColor="text-gray-500"
+                    />
+                    <LinkWithMessage
+                        href={`/dreams/${id}/delete`}
+                        icon={faDelete}
+                        textColor="text-black"
+                        danger
+                    />
+                </div>
+            </div>
+            <p className="text-md text-gray-700 leading-relaxed font-light italic">
+                {dream.description}
+            </p>
+            <div className="flex flex-wrap gap-2 m-2">
+                {themes.map(theme => (
+                    <span
+                        key={theme.theme}
+                        className="bg-brand-softer border border-brand-subtle bg-gray-300 text-fg-brand-strong text-sm font-medium px-2 py-1 rounded"
+                    >
+                    {theme.theme}
+                    </span>
+                ))}
+            </div>
+        </div>
+    )
+}
