f40b48e97bee04cf6b0dd2f66f7126ff76b25552
Alex Small <alex.small739@gmail.com>
2026-03-13 14:09:13 +0000

    Create StickyNote component to display and edit dream notes

---
+++ src/components/dreams/StickyNote.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import IconWithTooltip from "../ui/IconWithTooltip";
+import { faNoteSticky as faNote, faX, faCheck,faArrowRotateLeft as faUndo } from "@fortawesome/free-solid-svg-icons";
+
+export default function StickyNote ({notes, submitNewNote}:{notes: string | null, submitNewNote: (notes: string | null) => Promise<void>}) {
+
+    const [showUndo, setShowUndo] = useState(false)
+    const [newNote, setNewNote] = useState<string | null>(() => notes ?? null)
+    const [showNote, setShowNote] = useState(notes !== null)
+    const [saved, setSaved] = useState(false)
+
+    const handleNoteChange = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewNote(e.target.value)
+        setShowUndo(true)
+    }
+
+    const handleUndoEditNote = () => {
+        setNewNote(notes || null)
+        setShowUndo(false)
+    }
+
+    const handleClose = () => {
+        setShowNote(false)
+    }
+    
+    const handleUpdate = async () => {
+        await submitNewNote(newNote)
+        setSaved(true)
+        setTimeout(() => {
+            setShowUndo(false)
+            setSaved(false)
+            if (!newNote){
+                setShowNote(false)
+            }
+        }, 2000)
+    }
+
+    return (
+        <>{showNote?
+                <div className="absolute -bottom right-6 rotate-2 bg-yellow-200 p-3 shadow-lg rounded-sm w-48 min-h-40 max-h-64 hover:-translate-y-1 hover:shadow-xl transition">
+                        <div className="absolute -top-2 left-1/2 -translate-x-1/2 w-16 h-4 bg-yellow-100 opacity-70 rounded-sm shadow-sm"></div>
+                    <div className="absolute top-2 right-2 flex gap-1">
+                        <IconWithTooltip 
+                            tooltipText='Close' 
+                            icon={faX} 
+                            extraClass="text-xs hover:animate-pulse hover:cursor-pointer" 
+                            onClick={handleClose}
+                        />
+                        {showUndo && 
+                        <IconWithTooltip 
+                            tooltipText='Keep Changes' 
+                            icon={faCheck} 
+                            extraClass={`hover:animate-pulse text-xs hover:cursor-pointer ${saved ? 'text-green-500 animate-pulse' :''}`}
+                            onClick={handleUpdate}
+                        />}
+                        {showUndo && 
+                        <IconWithTooltip 
+                            tooltipText='Undo Changes' 
+                            icon={faUndo} 
+                            extraClass="text-xs hover:animate-pulse hover:cursor-pointer" 
+                            onClick={handleUndoEditNote}
+                        />}                        
+                    </div>
+                    <form className="absolute left-1 bottom-1 flex gap-1">
+                        <textarea 
+                            value={newNote || ''}
+                            onChange={handleNoteChange}
+                            className="w-44 h-32 px-2 bg-yellow-200 focus:outline-none text-gray-800 text-sm resize-none overflow-y-hidden"
+                        />
+                    </form>
+                </div>
+            :            
+                <div className="absolute bottom-4 right-6">
+                    <IconWithTooltip 
+                        icon={faNote}
+                        tooltipText={notes ? 'View Note': 'Add Note'}
+                        extraClass="text-2xl text-yellow-300 hover:text-yellow-400"
+                        onClick={() => setShowNote(true)}
+                    />
+                </div>
+        }</>
+    )
+}
+
