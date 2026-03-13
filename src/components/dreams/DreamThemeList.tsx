edd15b7468172f58f49e6821e7c2cf56c3136e27
Alex Small <alex.small739@gmail.com>
2026-03-13 14:55:20 +0000

    Add colours and absolute positioning to theme list for 'sticky label' style

--- src/components/dreams/DreamThemeList.tsx
+++ src/components/dreams/DreamThemeList.tsx
@@ -1,16 +1,26 @@
-import { ThemeResponse } from "@/types/themes";
-
 export default function DreamThemeList ({themes}:{themes:string[]}) {
+    const tagColors = [
+        "bg-cyan-400",
+        "bg-violet-400",
+        "bg-yellow-200",
+        "bg-pink-300",
+        "bg-teal-300",
+        "bg-indigo-400",
+    ]
+
     return (
-        <div className="flex flex-wrap gap-2 m-2">
-            {themes.map(theme => (
-                <span
-                    key={theme}
-                    className="bg-brand-softer border border-brand-subtle bg-gray-300 text-fg-brand-strong text-sm font-medium px-2 py-1 rounded"
-                >
-                {theme}
-                </span>
-            ))}
+        <div className="absolute right-[-40px] top-10 flex flex-col gap-2">
+            {themes.map((theme, index) => {
+                const color = tagColors[Math.min(index, 5)]
+                return (
+                    <span
+                        key={theme}
+                        className={`${color} text-xs px-2 py-1 shadow-xl`}
+                    >
+                    {theme}
+                    </span>
+                )
+            })}
         </div>
     )
 }
