4b8d4d685fda6eeed24d507693950c3082a1c1f9
Alex Small <alex.small739@gmail.com>
2026-03-16 19:03:53 +0000

    Add settingsName as prop to radio group

--- src/components/analyses/SettingsRadioGroup.tsx
+++ src/components/analyses/SettingsRadioGroup.tsx
@@ -1,25 +1,26 @@
 import { setterFunction } from "@/types/setterFunctions"
-import { Popover, PopoverButton, PopoverPanel, Field, RadioGroup, Radio, Label } from "@headlessui/react";
+import { Field, RadioGroup, Radio, Label } from "@headlessui/react";
 
 
 export default function SettingsRadioGroup({
-    setting, setSetting, options
+    settingName, setting, setSetting, options
 }:{
+    settingName: string
     setting: string,
     setSetting: setterFunction<string>,
     options: string[]
 }){
     return (
         <>
-            Tone:
-            <RadioGroup aria-label="Server size" value={setting} onChange={setSetting}>
+            <span className="font-bold">{settingName}:</span>
+            <RadioGroup aria-label="Server size" value={setting} onChange={setSetting} className='mb-2'>
             {options.map((option, index) => (
                 <Field key={index} className="flex items-center gap-2">
                 <Radio
                     value={option}
-                    className="group flex size-5 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-500 data-[checked]:border-blue-500"
+                    className="group flex size-3 items-center justify-center rounded-full border bg-white data-[checked]:bg-blue-500 data-[checked]:border-blue-500"
                 >
-                    <span className="invisible size-2 rounded-full bg-white group-data-[checked]:visible" />
+                    <span className="invisible size-1 rounded-full bg-white group-data-[checked]:visible" />
                 </Radio>
                 <Label>{option}</Label>
                 </Field>
