0657d5fc5f90f5ca0a54a56003151b1d1a3da191
Alex Small <alex.small739@gmail.com>
2026-03-18 16:29:08 +0000

    Remove brackets and count from selected value in dropdown

--- src/components/ui/Dropdown.tsx
+++ src/components/ui/Dropdown.tsx
@@ -13,13 +13,13 @@ export default function Dropdown<parameterType extends string>({
 
     return (
         <Menu as="div" className="relative inline-block">
-            <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs inset-ring-1 inset-ring-gray-300 hover:bg-gray-50">
-                {parameterName}: {parameter}
+            <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm shadow-xs inset-ring-1 inset-ring-gray-300 hover:bg-gray-50">
+                {parameterName}: {parameter.split(' ')[0]}
                 <ChevronDownIcon aria-hidden="true" className="-mr-1 size-5 text-gray-400" />
             </MenuButton>
             <MenuItems
                 transition
-                className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg outline-1 outline-black/5 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                className="absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg outline-1 outline-black/5 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
             >
                 <div className="py-1">
                     {options.map(option => 
