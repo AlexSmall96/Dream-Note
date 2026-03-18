9380a7d0895682b497116e2c1a867fed6bdc63b1
Alex Small <alex.small739@gmail.com>
2026-03-18 15:26:12 +0000

    Style search bar and results, add date to results

--- src/components/nav/SearchBar.tsx
+++ src/components/nav/SearchBar.tsx
@@ -1,5 +1,7 @@
 import { useDreams } from "@/contexts/DreamsContext"
 import { useThemesAside } from "@/contexts/ThemesAsideContext"
+import { formatDate } from "@/lib/utils/formatDate"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useRouter } from "next/navigation"
 
 export default function SearchBar() {
@@ -21,22 +23,23 @@ export default function SearchBar() {
 
     return (
         <div className="relative flex-1">
-            <form className="flex flex-col gap-2 border-2 border-gray-300 p-2">
+            <form className="flex flex-col gap-2 border-2 rounded border-gray-300">
                 <input 
                     type='text'
                     value={search}
                     onChange={handleSearch}
-                    placeholder='Search all dreams'
+                    placeholder='🔍︎ Search all dreams'
+                    className="p-2 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
                 />
                 {searchResults.length > 0 && 
-                    <div className="absolute top-full left-0 w-full bg-white border border-gray-300 z-10">
+                    <div className="absolute top-full left-0 w-full bg-white border border-gray-300 z-10 rounded">
                         {searchResults.map(dream => 
                             <div 
                                 key={dream._id} 
                                 onClick={() => handleClick(dream._id)} 
-                                className="hover:underline cursor-pointer"
+                                className="grid grid-cols-3 hover:bg-gray-100 cursor-pointer px-2 justify-between items-center"
                             >
-                                {dream.title}
+                                <span className="col-span-2">{dream.title}</span> <span className="col-span-1 text-xs text-gray-500">{formatDate(dream.date, true, true)}</span>
                             </div>
                         )}      
                     </div>
