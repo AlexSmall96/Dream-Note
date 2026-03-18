95f02c65d3c886ed625413e549fd486c3519b84e
Alex Small <alex.small739@gmail.com>
2026-03-18 07:33:17 +0000

    Remove date from dream if used in theme view, make title playwrite font

--- src/components/nav/DreamsList.tsx
+++ src/components/nav/DreamsList.tsx
@@ -34,8 +34,8 @@ export default function DreamsList(){
             {dreams.length > 0 && (
             <div className="grid grid-cols-3 gap-1">
                 {dreamsList.map(dream => (
-                    <div key={dream._id} className="contents">
-                        <div>{formatDate(dream.date)}</div>
+                    <div key={dream._id} className="contents font-playwrite">
+                        {view === 'dreams' && <div>{formatDate(dream.date)}</div>}
                         <div
                             onClick={() => handleClick(dream._id)}
                             className="col-span-2 hover:underline cursor-pointer"
