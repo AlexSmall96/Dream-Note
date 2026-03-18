3b7b7e2029b33d065dca56decc654141ffa47fbe
Alex Small <alex.small739@gmail.com>
2026-03-18 07:51:29 +0000

    Add colour to themes list using getColorForTheme

--- src/components/nav/ThemesList.tsx
+++ src/components/nav/ThemesList.tsx
@@ -1,5 +1,6 @@
 import { useThemesAside } from "@/contexts/ThemesAsideContext"
 import { useThemes } from "@/contexts/ThemesContext"
+import { getColorForTheme } from "@/lib/utils/getColorForTheme"
 import { getUniqueThemes } from "@/lib/utils/getUniqueThemes"
 
 export default function ThemesList () {
@@ -9,10 +10,10 @@ export default function ThemesList () {
     const {setSelectedTheme} = useThemesAside()
 
     return (
-        <div>
+        <div className='max-h-64 overflow-y-auto grid grid-cols-3 gap-1'>
             {uniqueThemes.map(theme =>
                 <div key={theme}>
-                    <button className='bg-blue-200 m-1 p-2' onClick={() => setSelectedTheme(theme)}>{theme} ({counts[theme]})</button>
+                    <button className={`font-caveat ${getColorForTheme(theme)} px-3 py-1 shadow-md border-l-4 border-black/20`} onClick={() => setSelectedTheme(theme)}>{theme} ({counts[theme]})</button>
                 </div> 
             )}
         </div>
