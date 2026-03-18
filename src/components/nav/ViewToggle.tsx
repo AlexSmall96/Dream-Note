aeb7bf8f7e1fdb0085a20c18e5f1c8cb6ebd109d
Alex Small <alex.small739@gmail.com>
2026-03-18 19:23:37 +0000

    Dont clear selected theme when view is changed

--- src/components/nav/ViewToggle.tsx
+++ src/components/nav/ViewToggle.tsx
@@ -5,31 +5,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function ViewToggle() {
     
-    const { setSelectedTheme, setView } = useThemesAside()
-
-    const handleChangeView = (view: 'dreams' | 'themes') => {
-        setView(view)
-        setSelectedTheme(null)
-    }
+    const { setView } = useThemesAside()
 
     return (
         <TabGroup className='flex flex-col gap-2 w-full'>
             <div className="flex items-center gap-2 w-full">
-            <span className='text-sm pr-2'>
-                <FontAwesomeIcon icon={faBrowse} className="text-gray-500" /> Browse:
-            </span>
-            <TabList className="flex gap-0 bg-gray-100 p-1 rounded-lg text-sm flex-1">
-                <Tab 
-                onClick={() => handleChangeView('dreams')}
-                className="flex-1 px-3 py-1 text-sm rounded-full data-[hover]:underline data-[selected]:bg-purple-400 data-[selected]:text-white flex items-center justify-center">
-                By Date
-                </Tab>
-                <Tab 
-                onClick={() => handleChangeView('themes')}
-                className="flex-1 px-3 py-1 text-sm rounded-full data-[hover]:underline data-[selected]:bg-purple-400 data-[selected]:text-white flex items-center justify-center">
-                By Theme
-                </Tab>
-            </TabList>
+                <span className='text-sm pr-2'>
+                    <FontAwesomeIcon icon={faBrowse} className="text-gray-500" /> Browse:
+                </span>
+                <TabList className="flex gap-0 bg-gray-100 p-1 rounded-lg text-sm flex-1">
+                    <Tab 
+                        onClick={() => setView('dreams')}
+                        className="flex-1 px-3 py-1 text-sm rounded-full data-[hover]:underline data-[selected]:bg-purple-400 data-[selected]:text-white flex items-center justify-center"
+                    >
+                        By Date
+                    </Tab>
+                    <Tab 
+                        onClick={() => setView('themes')}
+                        className="flex-1 px-3 py-1 text-sm rounded-full data-[hover]:underline data-[selected]:bg-purple-400 data-[selected]:text-white flex items-center justify-center"
+                    >
+                        By Theme
+                    </Tab>
+                </TabList>
             </div>
         </TabGroup>
     )
