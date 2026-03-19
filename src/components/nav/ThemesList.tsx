12e1a117cd420e995933d22f50373478e90ae884
Alex Small <alex.small739@gmail.com>
2026-03-19 15:48:15 +0000

    Make selected theme colour unmuted

--- src/components/nav/ThemesList.tsx
+++ src/components/nav/ThemesList.tsx
@@ -7,14 +7,14 @@ export default function ThemesList () {
 
     const { themes, counts } = useThemes()
     const uniqueThemes = getUniqueThemes(themes)
-    const {setSelectedTheme} = useThemesAside()
+    const {selectedTheme, setSelectedTheme} = useThemesAside()
 
     return (
         <div className='max-h-64 overflow-y-auto grid grid-cols-3 gap-1'>
             {uniqueThemes.map(theme =>
                 <div key={theme}>
                     <button 
-                        className={`${getColorForTheme(theme, true)} w-24 px-2 py-0.5 shadow-sm border-l-2 border-black/20`} 
+                        className={`${getColorForTheme(theme, selectedTheme !== theme)} w-24 px-2 py-0.5 shadow-sm border-l-2 border-black/20`} 
                         onClick={() => setSelectedTheme(theme)}
                     >
                         <span className="text-sm">{theme}</span> <span className="text-sm text-gray-500">({counts[theme]})</span></button>
