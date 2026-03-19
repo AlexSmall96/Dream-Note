535d67a27f2dcefb34677ec6237d541a2163c990
Alex Small <alex.small739@gmail.com>
2026-03-19 16:28:44 +0000

    Remove back to dashboard link from dream view

--- src/components/dreams/DreamView.tsx
+++ src/components/dreams/DreamView.tsx
@@ -49,11 +49,6 @@ export default function DreamView ({dreamId}:{dreamId:string}){
                 }
             </div>
             <div className="col-span-4 flex flex-col items-center">
-                <LinkWithMessage 
-                    href='/dreams'
-                    linkText="Back to Dashboard"
-                    extraClass="text-lg"
-                />
                 {loading ? 
                     <div className="flex justify-center items-center py-10">
                         <div className="w-6 h-6 border-2 border-purple-200 border-t-purple-500 rounded-full animate-spin"></div>
