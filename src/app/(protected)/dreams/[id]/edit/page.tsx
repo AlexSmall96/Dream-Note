ccc3fbe7fc5ea09a5dd7adb86b16b0ddb94e97ce
Alex Small <alex.small739@gmail.com>
2026-03-27 16:39:12 +0000

    Add useUpdateDream funciton to edit page, use dream data from DreamView context

--- src/app/(protected)/dreams/[id]/edit/page.tsx
+++ src/app/(protected)/dreams/[id]/edit/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { fetchFullDream  } from "@/lib/api/dreams";
 import DreamForm from "@/components/dreams/DreamForm";
 import { DreamFormType } from '@/types/dreams'
-import { useRouter } from "next/navigation"
-import { useDreamSubmit } from "@/app/hooks/useDreamSubmit";
-import { useThemes } from "@/contexts/ThemesContext";
+import { useUpdateDream } from "@/app/hooks/useUpdateDream";
+import { useDreamView } from "@/contexts/DreamViewContext";
 
 export default function EditDreamPage({
   	params,
@@ -14,48 +12,33 @@ export default function EditDreamPage({
   	params: { id: string };
 }) {
 	
-	const [dream, setDream] = useState<DreamFormType>({title: '', description: '', notes: '', date: ''})
-	const [themes, setThemes] = useState<string[]>([])
-	const { submitDream, msg, setMsg } = useDreamSubmit()
-	const { setRefetchThemes } = useThemes()
+	const [dreamFormData, setDreamFormData] = useState<DreamFormType>({title: '', description: '', notes: '', date: ''})
+	const { updateDream, msg, setMsg } = useUpdateDream()
+	const { dream, themes, setThemes} = useDreamView()
 	
-	const router = useRouter()
-
 	// Get existing dream
 	useEffect(() => {
-		const getDream = async () => {
-			const response = await fetchFullDream(params.id)
-			const { title, description, notes, date } = response.dream
-			setDream({ title, description: description || '', notes: notes || '', date: date.toISOString().split('T')[0]})
-			setThemes(response.themes?.map(theme => theme.theme)?? [])
-		} 
-		getDream()
-	}, [])
-
+		if (!dream || !dream.date) return
+		const { title, description, notes, date } = dream
+		setDreamFormData({ title, description: description || '', notes: notes || '', date: date.toISOString().split('T')[0]})
+	}, [dream])
+	
 	const handleSubmit = async (event: React.FormEvent) => {
 		event.preventDefault()
-		await submitDream({
-			id: params.id,
-			title: dream.title,
-			description: dream.description,
-			notes: dream.notes,
-			date: new Date(dream.date),
-			themes: themes
-		})		
-		setRefetchThemes(prev => !prev)
+		await updateDream(dreamFormData, themes, params.id)
 	}
 
   	return (
 		<div className="flex flex-col items-center m-4">
 			<DreamForm 
-				dream={dream} 
-				setDream={setDream}
+				dream={dreamFormData} 
+				setDream={setDreamFormData}
 				themes={themes}
 				setThemes={setThemes}
 				handleSubmit={handleSubmit}
 				msg={msg}
 				setMsg={setMsg}
-				handleGoBack={() => {router.replace(`/dreams/${params.id}`)}}
+				backHref={`/dreams/${params.id}`}
 				backText="Back to Dream"
 			/>
 		</div>
