75dfad86b1d41b832fd78743600e26a87f754845
Alex Small <alex.small739@gmail.com>
2026-03-27 16:37:21 +0000

    Create useUpdateDream hook for use in edit dream page

---
+++ src/app/hooks/useUpdateDream.ts
@@ -0,0 +1,23 @@
+"use client";
+import { DreamFormType } from "@/types/dreams";
+import { useDreamSubmit } from "@/app/hooks/useDreamSubmit";
+import { useThemes } from "@/contexts/ThemesContext";
+
+export function useUpdateDream() {
+    const { submitDream, msg, setMsg } = useDreamSubmit()
+    const { setRefetchThemes } = useThemes()
+
+    const updateDream = async (dream: DreamFormType, themes: string[],  id: string ) => {
+		await submitDream({
+			id,
+			title: dream.title,
+			description: dream.description,
+			notes: dream.notes,
+			date: new Date(dream.date),
+			themes: themes
+		})	
+        setRefetchThemes(prev => !prev)
+    }
+
+    return { updateDream, msg, setMsg }
+}
