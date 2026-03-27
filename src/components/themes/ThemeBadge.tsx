f8315f84378a96a93eda88e2a1528fd7ec412c64
Alex Small <alex.small739@gmail.com>
2026-03-27 14:32:26 +0000

    Move ThemeBadge into themes folder

--- src/components/themes/ThemeBadge.tsx
+++ src/components/themes/ThemeBadge.tsx
@@ -10,7 +10,7 @@ export function ThemeBadge({
 
     return(
         <span id="badge-dismiss-brand" 
-            className="inline-flex items-center bg-brand-softer border border-brand-subtle text-fg-brand-strong text-xs font-medium ps-1.5 pe-0.5 py-0.5 rounded gap-1">
+            className="inline-flex items-center bg-brand-softer bg-gray-300 px-2 py-1 mr-1 border border-brand-subtle text-fg-brand-strong text-xs font-medium rounded gap-1">
             {currentTheme}
             <button type="button" onClick={handleClick} className="inline-flex items-center p-0.5 text-sm bg-transparent rounded-xs hover:bg-neutral-tertiary" data-dismiss-target="#badge-dismiss-alternative" aria-label="Remove">
                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6"/></svg>
