2f98875342d5f6486a6bec9909bf9c35b134b218
Alex Small <alex.small739@gmail.com>
2026-04-01 16:00:52 +0100

    Update Analysis component to use as MainAnalysis view

--- src/components/analyses/Analysis.tsx
+++ src/components/analyses/Analysis.tsx
@@ -4,6 +4,7 @@ import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 import { DeleteModal } from "@/components/ui/DeleteModal";
 import { SavedAnalysis } from "@/types/aiAnalysis";
+import { useScreenSize } from '@/app/hooks/useScreenSize';
 
 export default function Analysis ({
     analysisData, onClickHeart, onClickText, onDelete, selected, clamp = true
@@ -17,9 +18,10 @@ export default function Analysis ({
 }) {
     const {tone, style, text, isFavorite, createdAt } = analysisData
     const formattedDate = format(new Date(createdAt), "PP")
+    const { isExtraLarge } = useScreenSize();
 
     return (
-        <div className={`flex items-center p-2 border-b ${selected? 'bg-gray-200': 'bg-white'} border-gray-200`}>
+        <div className={`flex items-center p-2 border-b ${selected && isExtraLarge? 'bg-gray-200': 'bg-white'} border-gray-200`}>
             <div className="flex-1 ml-2">
                 <div className="text-gray-500 text-sm flex gap-4 mt-1 items-center justify-between">
                     <div className="flex gap-2">
@@ -40,7 +42,11 @@ export default function Analysis ({
                 </div>
                 <div 
                     onClick={onClickText || undefined} 
-                    className={`text-gray-800 font-medium pr-2 text-justify ${clamp ? 'line-clamp-3':'max-h-[300px] overflow-y-auto scrollbar-custom'} ${!selected && onClickText ? 'hover:underline cursor-pointer' : ''}`}
+                    className={`
+                        text-gray-800 font-medium pr-2 text-justify 
+                        ${clamp ? 'line-clamp-3':'max-h-[300px] overflow-y-auto scrollbar-custom'} 
+                        ${(!selected || !isExtraLarge) && onClickText ? 'hover:underline cursor-pointer' : ''}
+                    `}
                 >
                     {text}
                 </div>
