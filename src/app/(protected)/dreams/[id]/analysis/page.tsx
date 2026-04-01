90d366ec91e91eb61d69f8550f05f695f743a2e8
Alex Small <alex.small739@gmail.com>
2026-04-01 15:56:57 +0100

    Update breadcrumb text size and spacing for small/large screens

--- src/app/(protected)/dreams/[id]/analysis/page.tsx
+++ src/app/(protected)/dreams/[id]/analysis/page.tsx
@@ -4,6 +4,7 @@ import { AnalysesProvider } from "@/contexts/AnalysesContext";
 import { fetchFullDream } from "@/lib/api/dreams";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useScreenSize } from '@/app/hooks/useScreenSize';
 
 export default function DreamAnalysisPage ({ params }: { params: { id: string } }) {
 
@@ -11,6 +12,7 @@ export default function DreamAnalysisPage ({ params }: { params: { id: string } 
     const [description, setDescription] = useState('')
     const [title, setTitle] = useState('')
     const [showMainAnalysis, setShowMainAnalysis] = useState(false)
+    const { isExtraLarge } = useScreenSize();
     
     const router = useRouter()
 
@@ -37,32 +39,43 @@ export default function DreamAnalysisPage ({ params }: { params: { id: string } 
 
     return (
         <div className="flex flex-col mt-0">
-            <div className="flex items-center gap-2 w-full px-6 h-14">
-            <button
-                onClick={() => {
-                    if (showMainAnalysis) {
-                        setShowMainAnalysis(false) 
-                    } else {
-                        router.push(`/dreams/${params.id}`) 
-                    }
-                }}
-                className="text-gray-600 hover:underline flex items-center gap-1"
-            >
-                ←
-            </button>
+            <div className="flex justify-between gap-2 w-full px-2 text-sm py-4 md:h-14 md:text-md lg:text-lg w-auto">
+                <div className="flex flex-wrap items-center gap-1 text-gray-700">
+                    <button
+                        onClick={() => {
+                            if (showMainAnalysis) {
+                                setShowMainAnalysis(false) 
+                            } else {
+                                router.push(`/dreams/${params.id}`) 
+                            }
+                        }}
+                        className="text-gray-600 hover:underline flex items-center gap-1"
+                    >
+                        ←
+                    </button>
 
-            <div className="flex items-center gap-1 text-gray-700 truncate">
-                <button 
-                    className="truncate max-w-[120px] hover:underline"
-                    onClick={() => router.push(`/dreams/${params.id}`)}
-                    
-                    >{title}</button>
-                <span>/</span>
-                <button onClick={() => setShowMainAnalysis(false)} className={!showMainAnalysis ? "font-semibold" : "hover:underline"}>Analyses</button>
-                {showMainAnalysis &&<span>/</span>}
-                {showMainAnalysis &&<span className="font-semibold">Selected Analysis</span>}
+                    <div className="flex items-center gap-1 text-gray-700">
+                        <button 
+                            className="truncate max-w-[120px] hover:underline"
+                            onClick={() => router.push(`/dreams/${params.id}`)}
+                            
+                            >{title}</button>
+                        <span>/</span>
+                        <button onClick={() => setShowMainAnalysis(false)} className={!showMainAnalysis || isExtraLarge ? "font-semibold" : "hover:underline"}>Analyses</button>
+                        {showMainAnalysis && !isExtraLarge && 
+                        <>
+                            <span>/</span>
+                            <span className="font-semibold">Selected Analysis</span>
+                        </>
+                        }
+                        
+                    </div>
+                </div>
+                {isExtraLarge && 
+                    <div className="flex items-center font-semibold">Selected Analysis</div>
+                }
             </div>
-            </div>
+            
             {/* Wrap component in a provider to avoid props drilling to sub components */}
             <AnalysesProvider dreamId={params.id} title={title} description={description} >
                 <AnalysesView showMainAnalysis={showMainAnalysis} setShowMainAnalysis={setShowMainAnalysis}/>
