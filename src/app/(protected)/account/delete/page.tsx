49e3c6256e0ecbf76a79220c362819361019c197
Alex Small <alex.small739@gmail.com>
2026-04-06 11:05:07 +0100

    Add w-full and max-width to DeleteAccount

--- src/app/(protected)/account/delete/page.tsx
+++ src/app/(protected)/account/delete/page.tsx
@@ -4,8 +4,8 @@ import DeleteAccount from "@/components/account/DeleteAccount";
 export default function DeleteAccountPage(){
 
     return (
-        <div className="flex flex-col items-center m-5">
-            Confirm your password to delete account.
+        <div className="flex flex-col items-center mt-2">
+            <h1 className="text-lg font-bold text-center h-16 px-2 md:h-12">Confirm your password to delete account</h1>
             <DeleteAccount />
         </div>
     )
