85cc932aa49ebb17bb9242fd4e22640e2660d59b
Alex Small <alex.small739@gmail.com>
2026-04-06 11:04:28 +0100

    Wrap account page components in Card

--- src/app/(protected)/account/page.tsx
+++ src/app/(protected)/account/page.tsx
@@ -3,6 +3,7 @@ import PasswordForm from "@/components/account/PasswordForm";
 import UpdateEmailLink from "@/components/account/UpdateEmailLink";
 import VerifyEmail from "@/components/account/VerifyEmail";
 import LinkWithMessage from "@/components/forms/LinkWithMessage";
+import { Card } from "@/components/ui/Card";
 import { useCurrentUser } from "@/contexts/CurrentUserContext";
 import { faTriangleExclamation as faWarn } from "@fortawesome/free-solid-svg-icons";
 
@@ -12,25 +13,27 @@ export default function Account(){
     const isVerified = currentUser?.isVerified
 
     return (
-        <div className="flex flex-col items-center">
-            <h1 className="text-l font-bold m-4">Account</h1>
-            {!isGuest ?
-            <>
-                <UpdateEmailLink />
-                {isVerified ? 
+        <div className="flex flex-col items-center mt-2">
+            <h1 className="text-lg font-bold text-center h-12">Account</h1>
+            <Card className="w-full max-w-xl mt-0.5">
+                {!isGuest ?
                 <>
-                    <PasswordForm />
-                    <LinkWithMessage href='/account/delete' linkText='Delete Account' icon={faWarn} danger />
-                </> 
-                :
-                    <VerifyEmail />
-                }
-            </>
-            : 
-                <>
-                    Email and password updates and account deletion are disabled for guests. Create an account to access these features.
+                    <UpdateEmailLink />
+                    {isVerified ? 
+                    <>
+                        <PasswordForm />
+                        <LinkWithMessage href='/account/delete' linkText='Delete Account' icon={faWarn} danger />
+                    </> 
+                    :
+                        <VerifyEmail />
+                    }
                 </>
-            }
+                : 
+                    <>
+                        <p className="text-center">Email and password updates and account deletion are disabled for guests. Create an account to access these features.</p>
+                    </>
+                }
+            </Card>
         </div>
     )
 }
