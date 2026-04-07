2fa756810fb5f9391a9ea2feaba1c0e465b00831
Alex Small <alex.small739@gmail.com>
2026-04-07 13:51:24 +0100

    Add selected theme button to AsideContent

--- src/components/nav/AsideContent.tsx
+++ src/components/nav/AsideContent.tsx
@@ -5,10 +5,11 @@ import ThemesList from '@/components/nav/ThemesList';
 import { useCurrentUser } from '@/contexts/CurrentUserContext';
 import { useThemesAside } from '@/contexts/ThemesAsideContext';
 import ViewToggle from './ViewToggle';
+import { getColorForTheme } from '@/lib/utils/getColorForTheme';
 
 export function AsideContent() {
 
-    const { selectedTheme, view } = useThemesAside()
+    const { selectedTheme, setSelectedTheme, view } = useThemesAside()
     const { loading, currentUser } = useCurrentUser()
 
     if (loading || !currentUser) return null
@@ -16,8 +17,16 @@ export function AsideContent() {
     return (
         <div className="p-3 w-full flex flex-col gap-2">
             <ViewToggle />
+            {selectedTheme && view === 'themes' && 
+                <div className="flex items-center gap-2">
+                    <button className='mr-1' onClick={() => setSelectedTheme('')}>
+                        ←
+                    </button>
+                    <span className={`${getColorForTheme(selectedTheme, true)} text-sm w-auto px-2 py-1 shadow-sm border-l-2 border-black/20`}>{selectedTheme}</span>
+                </div>
+            }
             <div className='bg-white p-2 rounded border border-gray-200 shadow-sm mt-0.5'>
-                {view === 'themes' && <ThemesList />}
+                {view === 'themes' && !selectedTheme && <ThemesList />}
                 {view === 'themes' && selectedTheme && <DreamsList />}
                 {view === 'dreams' && <MonthsWithDreams />}
             </div>
