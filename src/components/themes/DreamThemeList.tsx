2601bd1c88be1f31a4e65c0aece9188fe651167b
Alex Small <alex.small739@gmail.com>
2026-04-08 13:45:27 +0100

    Add max width to text version of themes

--- src/components/themes/DreamThemeList.tsx
+++ src/components/themes/DreamThemeList.tsx
@@ -12,24 +12,25 @@ export default function DreamThemeList () {
 
     return (
         <div className="
-            absolute bottom-4 flex flex-wrap gap-2 md:w-2/5 lg:w-1/2 pr-8
+            absolute bottom-4 flex flex-wrap gap-2 md:w-3/5 lg:w-1/2 pr-8 sm:w-2/5 w-3/5
             xl:right-[-65px] xl:top-16 xl:flex-col xl:w-20 xl:min-w-0"
         >
             {themes.map((theme, index) => {
                 const color = getColorForTheme(theme)
                 const isPeeling = peelingTheme === theme
                 return (
-                    <>{isLargeAndAbove?
+                    <div key={theme}>
+                    {isLargeAndAbove?
                         <ThemeLabel 
-                            key={index} 
                             theme={theme} 
                             color={color} 
                             isPeeling={isPeeling} 
                             index={index} 
                         />
                     :
-                        <span className='text-gray-400'>{theme}</span>
-                    }</>
+                        <span className='pr-1 text-gray-400 max-w-[80px] truncate block'>{theme}</span>
+                    }
+                    </div>
                 )
             })}
             {showBlankLabel && isLargeAndAbove && <BlankLabel />}
