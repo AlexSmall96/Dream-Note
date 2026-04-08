82832d52c534bbc6a47ba3f2efd8ef7626b57e3a
Alex Small <alex.small739@gmail.com>
2026-04-08 20:09:20 +0100

    Use custom Button component, style heading, add Back to Dashboard text.

--- src/components/dreams/DeleteDream.tsx
+++ src/components/dreams/DeleteDream.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/navigation";
+import Button from "../forms/Button";
 
 
 export default function DeleteDream ({
@@ -9,16 +10,16 @@ export default function DeleteDream ({
     const router = useRouter()
 
     return (
-        <div className="flex flex-col items-center">
-            <h1>{msg}</h1>
-            <button 
+        <div className="flex flex-col items-center mt-4">
+            <h1 className="text-xl font-semibold mb-4 text-center px-2">{msg}</h1>
+            <div className="flex gap-2">
+            <Button 
                 onClick={() => router.replace(backUrl)} 
-                className='bg-gray-500 p-2'
-            >
-                {deleted? 'Back to all dreams': 'Go back'}
-            </button>
-            
-            {visible && <button onClick={handleDelete} className='bg-red-500 p-2' disabled={waiting}>Yes</button>}
+                extraClass='bg-gray-500 p-2'
+                text={deleted? 'Back to Dashboard': 'Go back'}
+            />
+            {visible && <Button onClick={handleDelete} extraClass='bg-red-500 p-2' text="Yes" disabled={waiting} />}
+            </div>
         </div>
     )
 }
