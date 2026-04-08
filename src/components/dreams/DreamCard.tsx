75ddaa3deecab24774ea496880659888b83bd8e5
Alex Small <alex.small739@gmail.com>
2026-04-08 13:44:47 +0100

    Hide new theme icon for small screens

--- src/components/dreams/DreamCard.tsx
+++ src/components/dreams/DreamCard.tsx
@@ -4,12 +4,13 @@ import { useParams } from "next/navigation"
 import IconWithTooltip from '@/components/ui/IconWithTooltip';
 import StickyNote from '@/components/dreams/StickyNote';
 import DreamThemeList from '@/components/themes/DreamThemeList';
+import { useScreenSize } from "@/app/hooks/useScreenSize";
 
 export default function DreamCard () {
     const { dream, setShowBlankLabel } = useDreamView()
     const params = useParams()
     const id = params.id as string
-
+    const { isLargeAndAbove } = useScreenSize()
     return (
         <div className="font-caveat relative mb-8 pt-8 px-6 pb-20 bg-[url('/images/paper.jpg')] bg-cover bg-center rounded-lg shadow-lg border border-purple-100 w-full max-w-3xl h-110">
             <div className="flex justify-between items-start mb-4">
@@ -41,15 +42,17 @@ export default function DreamCard () {
                         extraClass="text-xl"
                         danger
                     />
-                    <IconWithTooltip
-                        icon={faTheme}
-                        onClick={() => setShowBlankLabel(true)}
-                        tooltipText="New Theme"
-                        extraClass="text-xl text-indigo-400"
-                    />
+                    {isLargeAndAbove && dream.description && (
+                        <IconWithTooltip
+                            icon={faTheme}
+                            onClick={() => setShowBlankLabel(true)}
+                            tooltipText="New Theme"
+                            extraClass="text-xl text-indigo-400"
+                        />
+                    )}
                 </div>
             </div>
-            <p className="text-md text-xl text-gray-700 leading-relaxed font-light italic overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100 max-h-60 mb-4 pb-4 pr-5 text-justify">
+            <p className="text-md text-xl text-gray-700 leading-relaxed font-light min-h-[150px] italic overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100 max-h-60 mb-4 pb-4 pr-5 text-justify">
                 {dream.description}
             </p>
             <DreamThemeList />
