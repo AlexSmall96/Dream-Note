fa5b20479c417277b54c0805a492567e9e31e266
Alex Small <alex.small739@gmail.com>
2026-04-10 08:45:46 +0100

    Use gray scrollbar on DescriptionSnapshot and DreamCard

--- src/components/analyses/DescriptionSnapshot.tsx
+++ src/components/analyses/DescriptionSnapshot.tsx
@@ -4,7 +4,7 @@ import { useAnalysesContext } from "@/contexts/AnalysesContext";
 export default function DescriptionSnapshot() {
     const { description } = useAnalysesContext();
     return (
-        <Card className="bg-paper max-h-[40vh] overflow-y-auto scrollbar-custom text-justify">
+        <Card className="bg-paper max-h-[40vh] overflow-y-auto scrollbar-custom-gray text-justify">
             <p className='font-semibold mt-2'>Description used:</p>
             <p className="mt-1 text-lg font-caveat">{description}</p>
         </Card>
