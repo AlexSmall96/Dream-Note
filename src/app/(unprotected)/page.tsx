f57b5dec279b7be4192a7ce2d50b3e266391376b
Alex Small <alex.small739@gmail.com>
2026-04-24 12:00:47 +0100

    Remove header from root page

--- src/app/(unprotected)/page.tsx
+++ src/app/(unprotected)/page.tsx
@@ -4,9 +4,6 @@ import React from 'react';
 export default  function Home() {
 
   	return (
-		<div className="flex flex-col items-center m-4">
-			<h1>Home page</h1>  
-			<LandingPage />
-		</div>
+		<LandingPage />
   	);
 }
