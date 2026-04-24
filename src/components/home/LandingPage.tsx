fa36d5a9fc3d17213bc20f33ee6c6008d4ecfd08
Alex Small <alex.small739@gmail.com>
2026-04-24 12:01:17 +0100

    Add responsive images and text to landing page

--- src/components/home/LandingPage.tsx
+++ src/components/home/LandingPage.tsx
@@ -3,6 +3,8 @@ import { useCurrentUser } from "@/contexts/CurrentUserContext"
 import { loginGuest } from "@/lib/api/auth"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
+import Image from 'next/image';
+import Button from "../forms/Button"
 
 export default function LandingPage(){
 
@@ -29,11 +31,90 @@ export default function LandingPage(){
     }
 
     return (
-        <>
-            <a href='/auth/login' className="font-medium text-fg-brand hover:underline text-blue-500">Login</a>
-            <a href='/auth/signup' className="font-medium text-fg-brand hover:underline text-blue-500">Signup</a>
-            or
-            <button type="button" onClick={handleLoginGuest} className='bg-gray-500 hover:bg-gray-700 text-white font-bold p-1'>Continue as Guest</button>
-        </>
+        <div className="flex flex-col items-center mt-4">
+            {/* HERO */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center w-full px-6 md:px-12 lg:px-24">
+                <div className="flex justify-center md:order-1 order-2">
+                    <Image 
+                        src="/images/dream-card.png" 
+                        alt="Dream card example" 
+                        width={600}
+                        height={600}
+                        className="opacity-60 -rotate-6 hover:scale-105 transition w-[400px] md:w-[400px] lg:w-[600px]"
+                    />
+                </div>
+                <div className="flex flex-col items-center md:order-2 order-1 md:items-start text-center md:text-left">
+                    <h1 className="header-text flex items-center justify-center md:justify-start gap-2">
+                        <Image src="/images/moon.png" alt="moon icon" width={100} height={100}/>
+                        <span>Your Dreams, Organized</span>
+                    </h1>
+                    <p className="max-w-lg mb-4">
+                        DreamNote is your personal dream journal. Record your dreams, organize by date and themes, and gain insights with AI-assisted analysis.
+                    </p>
+                    <div className="flex items-center gap-2 flex-wrap justify-center md:justify-start">
+                        <Button text="Get Started" onClick={() => router.replace('/auth/signup')} />
+                        <span>or</span>
+                        <Button text="Continue as Guest" onClick={handleLoginGuest} color="bg-gray-500 hover:bg-gray-700"/>
+                    </div>
+                </div>
+            </div>
+            {/* SECOND SECTION */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center mt-12 px-6 md:px-12 lg:px-24">
+                <div className="flex flex-col items-center md:items-end text-center md:text-right">
+                    <h1 className="header-text flex items-center justify-center md:justify-end gap-2">
+                        <span>AI Assisted Features</span>
+                        <Image src="/images/ai.png" alt="book icon" width={80} height={80}/>
+                    </h1>
+                    <p className="max-w-md mb-4">
+                        Optimize your dream tracking with AI generated themes, title suggestions and analysis.
+                    </p>
+                    <p>
+                        Already have an account? 
+                        <a href="/auth/login" className="text-blue-500 hover:underline ml-1">Login</a>
+                    </p>
+                </div>
+                <div className="flex justify-center md:justify-end w-full">
+                    <Image 
+                        src="/images/analysis.png"
+                        alt="Analysis example"
+                        width={600}
+                        height={600}
+                        className="opacity-60 hover:scale-105 transition"
+                    />
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center mt-12 px-6 md:px-12 lg:px-24">
+                {/* Image */}
+                <div className="flex justify-center md:order-1 order-2">
+                    <div className="flex flex mx-4">
+                        <Image 
+                            src="/images/themes.png"
+                            alt="Organize dreams by date and theme"
+                            width={200}
+                            height={200}
+                            className="opacity-60 hover:scale-105 transition"
+                        />
+                        <Image 
+                            src="/images/dates.png"
+                            alt="Organize dreams by date and theme"
+                            width={200}
+                            height={200}
+                            className="opacity-60 hover:scale-105 transition"
+                        />
+                    </div>
+
+                </div>
+                {/* Text */}
+                <div className="flex flex-col items-center md:order-2 order-1 md:items-start text-center md:text-left ml-4">
+                    <h2 className="header-text flex items-center justify-center md:justify-start gap-2">
+                        <Image src="/images/book.png" alt="book icon" width={100} height={100}/>
+                        <span>Organise by Date & Theme</span>
+                    </h2>
+                    <p className="max-w-md mt-2 ml-4">
+                        Quickly find patterns in your dreams by browsing chronologically or exploring recurring themes.
+                    </p>
+                </div>
+            </div>
+        </div>
     )
 }
